adaec511692ce96ac1bc5c6ebde203255a667efe
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-01 19:40:10 -0300

    add base class 01 - Data Types

---
+++ JavaScript/class-01-data-types/script.js
@@ -0,0 +1,72 @@
+// Segundo o ECMAScript
+
+//  *Primitive Values : String, Number, Boolean...
+
+// string 
+// nome = "Emanuel"
+// console.log(typeof "Emanuel")
+
+// number: integer, float, NaN, Infinity
+// console.log(typeof 32.2)
+// console.log(typeof (32 + 5))
+// console.log(15/"Emanuel")
+// console.log(typeof NaN)
+
+// boolean: true or false
+// console.log(typeof (12 == 13))
+// console.log(20 == 20)
+// console.log(12 == "Emanuel")
+// console.log((11 === "11")) //cuidado porque são tipos diferentes de dados
+
+// num1 = "11"
+// num2 = 22
+// soma = num1 + num2
+
+// console.log(soma)
+
+//  *Tipos Estrutural: Object, Function
+
+// object
+
+// aluno = {
+//     nome:"Emanuel",
+//     idade: 28,
+// }
+
+// console.log(typeof aluno)
+// console.log(aluno.nome)
+
+// array or vetor 
+
+// lista = ["Leite", 30, "pão", 20]
+
+// console.log(typeof lista)
+// console.log(typeof lista[0])
+// console.log(typeof lista[1])
+
+
+// let colege = []
+
+// let studant1 = {
+//     nome: "Emanuel",
+//     age: 29,
+//     wight: 1.85,
+//     isSubscribed: true
+// }
+// const studant2 = {
+//     nome: "João",
+//     age: 28,
+//     wight: 1.80,
+//     isSubscribed: true
+// }
+
+// console.log(typeof colege)
+// colege[0] = studant1
+// colege[1] = studant2 
+// console.log(colege)
+// console.log(colege[0])
+// console.log(colege[1])
+
+// console.log(studant)
+// console.log(`${studant.nome} de idade de ${studant.age} anos pesa ${studant.wight}`)
+// console.log(studant.nome + " de idade de " + studant.age + " anos pesa " + studant.wight)

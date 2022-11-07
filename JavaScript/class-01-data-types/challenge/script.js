5709d7717467009797e2cb3504c2e93d1d75704e
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-07 07:58:43 -0300

    update class 01 challenge

--- JavaScript/class-01-data-types/challenge/script.js
+++ JavaScript/class-01-data-types/challenge/script.js
@@ -1,13 +1,12 @@
-// Faça um programa que solicite (prompt) o nome do usuáio e dê as boas vindas: Ex: Bem-vindo "nome"!
-// Solicite dois números usando o nome do usuário Ex: Digite dois números "nome"
+// Faça um programa que solicite (prompt) o nome do usuáio, dê as boas vindas e avise que irá fazer uma soma: Ex: Bem-vindo "nome", vamos fazer uma soma!
 // Apresente a soma dos números com alerta na tela Ex: A soma do "número1" e o "número2" é "soma". (Use Template String) 
 
-let name = prompt("Digite seu nome: ");
+let userName = prompt("Digite seu nome: ");
 
-alert("Olá, " + name);
+alert("Olá, " + userName + ", vamos fazer uma soma!");
 
-let firstNumber = Number(prompt("Digite o primeiro número: "));
-let secondNumber = Number(prompt("Digite o sugundo número: "));
+let firstNumber = Number(prompt("O primeiro número: "));
+let secondNumber = Number(prompt("O sugundo número: "));
 
 const sum = firstNumber + secondNumber;
 

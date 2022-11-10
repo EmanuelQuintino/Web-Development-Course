eb7a95fd7b91139d596d05f64a1c831bd8addd0f
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-10 09:33:09 -0300

    add class 03 functions

---
+++ JavaScript/class-03-functions/study/script.js
@@ -0,0 +1,48 @@
+// Function Declarations
+
+// >>>> Nomeada <<<<
+ 
+// function myTest(text) { //parameters
+//     console.log(text)
+// }
+
+// myTest("Testando a função") //arguments
+
+
+// function somar(num1, num2) {
+//     // return num1 + num2
+
+//     let soma = num1 + num2 //Atenção para o operador chave
+//     return soma
+// }
+
+
+// >>>> Anonima <<<<
+
+// const somar = function (num1, num2) {
+//     return num1 + num2
+// }
+
+
+// >>>>> Arrow <<<<<
+
+const somar = (num1, num2) => num1 + num2
+
+// console.log(somar(2, 5))
+// console.log(soma)
+
+
+// >>>> Function() Constructor <<<<<
+
+// function Car(brandName, motorPower) {
+//     this.brand = brandName
+//     this.motor = motorPower
+// }
+
+// const siena = new Car("Fiat", 1.4)
+// const onix = new Car("Chevrolet", 1.4)
+// const polo = new Car("Volkswagen", 1.0)
+
+// console.log(siena.brand)
+// console.log(onix.motor)
+// console.log(polo)

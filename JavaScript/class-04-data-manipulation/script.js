ab91077437e7aef5a60f378d284406c47202bda7
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-17 18:58:21 -0300

    update class date manipulation text

--- JavaScript/class-04-data-manipulation/script.js
+++ JavaScript/class-04-data-manipulation/script.js
@@ -73,45 +73,46 @@ let num2 = -5
 
 // >>>>>>>>>>>>>> Date Manipulation <<<<<<<<<<<<
 
-date = new Date()
-let date1 = new Date('2022-03-01 02:05:03')
-let date2 = new Date('2022-04-01 02:22:23')
+// let dateNow = new Date()
+// let date1 = new Date("2022-03-01 12:03:19")
+// let date2 = new Date("2022-04-01 12:03:19")
 
-// document.write(date)
+// document.write(dateNow)
+// let hour = dateNow.getHours()
+// let minutes = dateNow.getMinutes()
+// let seconds = dateNow.getSeconds()
+// let milliseconds = dateNow.getMilliseconds()
 
-// document.write(date1.getHours())
-// document.write(date1.getMinutes())
-// document.write(date1.getSeconds().toString().padStart(2, '0'))
+// document.write(`${String(hour).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}:${String(milliseconds).padStart(3, "0")}`)
 
-
-// document.write(date1.getHours().toString().padStart(2, '0') + ":" + date1.getMinutes().toString().padStart(2, '0') + ":" + date1.getSeconds().toString().padStart(2, '0'))
+// date1.setHours(date1.getHours() - 3)
+// date1.setMinutes(date1.getMinutes() + 60)
+// date1.setSeconds(date1.getSeconds() + 60)
 
 // document.write(date1.toLocaleTimeString("pt-BR"))
 
 
-// document.write(date.getDay()) // 0 - 6
-// document.write(date.getDate())
-// document.write(date.getMonth() + 1) // 0 - 11
-// document.write(date.getFullYear())
+// let day = dateNow.getDay()
+// let date = dateNow.getDate()
+// let month = dateNow.getMonth() + 1
+// let year = dateNow.getFullYear()
+// let minutes = date1.getMinutes()
+// let seconds = date1.getSeconds()
 
-// document.write(date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear())
+// document.write(day + 1) // 0 - 6
+// document.write(date)
+// document.write(month + 1) // 0 - 11
+// document.write(year)
 
-// document.write(date.toLocaleDateString("pr-BR"))
+// document.write(`${String(date).padStart(2, "0")}/${String(month).padStart(2, "0")}/${year}`)
 
+// dateNow.setDate(dateNow.getDate() + 30)
+// dateNow.setMonth(dateNow.getMonth() + 2)
+// dateNow.setFullYear(dateNow.getFullYear() + 30)
 
-// date1.setSeconds(date1.getSeconds() + 157)
-// date1.setMinutes(date1.getMinutes() + 56)
-// date1.setHours(date1.getHours() + 23)
+// document.write(dateNow.toLocaleDateString("pt-BR"))
 
-// date1.setDate(date1.getDate() + 20)
-// date1.setMonth(date1.getMonth() + 13)
-// date1.setFullYear(date1.getFullYear() + 20)
-
-// document.write(date1.toLocaleString("pr-BR"))
-
-
-                                                //   s     m    h    d
-// document.write(Math.floor(Math.abs(date1 - date2) / 1000 / 60 / 60 / 24))
+// document.write(dateNow.toLocaleString("pt-BR"))
 
 
 

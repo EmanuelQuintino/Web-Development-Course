d8e84786052a3f98074c1b7ad2c45ba4cb93bbd1
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-17 10:45:13 -0300

    add challenge digital clock

---
+++ JavaScript/challenges/04-data-manipulation/digital-clock/script.js
@@ -0,0 +1,12 @@
+const digitalClock = document.querySelector("#digitalClock")
+
+setInterval( () => {
+    let date = new Date()
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    let seconds = date.getSeconds()
+
+    digitalClock.innerHTML = `${String(hours).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`    
+})
+
+

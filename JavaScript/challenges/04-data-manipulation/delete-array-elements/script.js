20c3ef79427cd6c1e2e5d2fc722be81389ae7940
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-18 20:11:38 -0300

    update challenge delete array elements text

--- JavaScript/challenges/04-data-manipulation/delete-array-elements/script.js
+++ JavaScript/challenges/04-data-manipulation/delete-array-elements/script.js
@@ -1,5 +1,5 @@
 // Crie uma função que remova os elementos de um Array através do parâmetro
-// O algoritimo deve funcionar com tipo texto e tipo numérico
+// O algoritmo deve funcionar com tipo texto e tipo numérico
 // Valide para receber espaços, letras maiúsculas e letras minúsculas. 
 
 let techs = ['HTML', 15, 'CSS', 15, 'JavaScript', 30, 'SQL', 10, 'Git', 8]

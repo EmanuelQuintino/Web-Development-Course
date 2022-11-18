7c4d8283e1e3777391dee16ec052ba0563ca1bd5
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-18 19:54:42 -0300

    update challenge digital clock

--- JavaScript/challenges/04-data-manipulation/digital-clock/script.js
+++ JavaScript/challenges/04-data-manipulation/digital-clock/script.js
@@ -6,10 +6,11 @@ const digitalClock = document.querySelector("#digitalClock")
 
 setInterval( () => {
     let date = new Date()
-    let hour = date.getHours()
-    let minutes = date.getMinutes()
-    let seconds = date.getSeconds()
+    digitalClock.innerHTML = date.toLocaleTimeString()
+    
+    // let hour = date.getHours()
+    // let minutes = date.getMinutes()
+    // let seconds = date.getSeconds()
 
-    digitalClock.innerHTML = `${String(hour).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`
-
+    // digitalClock.innerHTML = `${String(hour).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`
 }, 100)

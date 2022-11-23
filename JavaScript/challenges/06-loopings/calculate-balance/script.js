0e5919c3a11500bfc1309c4537af5e952f79184f
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-23 13:01:12 -0300

    refactoring functions and console.log

--- JavaScript/challenges/06-loopings/calculate-balance/script.js
+++ JavaScript/challenges/06-loopings/calculate-balance/script.js
@@ -1,6 +1,7 @@
-// Crie uma função para calcular o saldo com dois argumentos: Receita e Despesas
-// Imprima o saldo formatado, se está Positivo ou Negativo e se existe dívidas.
-// Use as duas listas abaixo. Dica: Trate os dados com os métodos do array
+// Crie uma lógica para calcular o somatório das Receitas e das Despesas abaixo
+// Imprima os somatórios e o saldo formatado no padão brasileiro,
+// Informe se a situação está Positiva ou Negativa e se existe dívidas.
+// Dica: Trate os dados com os métodos do array
 
 const incomes = [
     'salary', 'R$ 2000,00',
@@ -15,20 +16,16 @@ const expenses = [
     'food', 'R$ 1300,62',
 ]
 
-function balence(incomes, expenses) {
-    const incomeValues = incomes.filter((value) => value.includes('R$'))
-    const incomeValuesNumber = incomeValues.map((income) => Number(income.replace("R$ ", "").replace(',', '.')))
-    const incomesSum = incomeValuesNumber.reduce((a, b) => a + b)
+const incomeValues = incomes.filter((value) => value.includes('R$'))
+const incomeValuesNumber = incomeValues.map((income) => Number(income.replace("R$ ", "").replace(',', '.')))
+const incomesSum = incomeValuesNumber.reduce((a, b) => a + b)
 
-    const expensesValues = expenses.filter((value) => value.includes('R$'))
-    const expensesValuesNumber = expensesValues.map((expense) => Number(expense.replace("R$ ", "").replace(',', '.')))
-    const expensesSum = expensesValuesNumber.reduce((a, b) => a + b)
-
-    return incomesSum - expensesSum
-}
+const expensesValues = expenses.filter((value) => value.includes('R$'))
+const expensesValuesNumber = expensesValues.map((expense) => Number(expense.replace("R$ ", "").replace(',', '.')))
+const expensesSum = expensesValuesNumber.reduce((a, b) => a + b)
 
 let statusBalence
-let resultBalance = balence(incomes, expenses)
+let resultBalance = incomesSum - expensesSum
 
 if ( resultBalance > 0) {
     statusBalence = 'Existe um superávit. Invista o dinheiro!'
@@ -39,4 +36,8 @@ else if ( resultBalance < 0) {
     statusBalence = 'Foi gasto todo dinheiro, mas não existe dívidas!'
 }
 
-console.log(`$Saldo de ${resultBalance.toLocaleString('pt-br', {style: 'currency', currency: 'brl'})}. ${statusBalence}`)
+console.log
+(`Gastos: ${incomesSum.toLocaleString('pt-br', {style: 'currency', currency: 'brl'})}
+Despesas: ${expensesSum.toLocaleString('pt-br', {style: 'currency', currency: 'brl'})}
+Saldo: ${resultBalance.toLocaleString('pt-br', {style: 'currency', currency: 'brl'})}
+${statusBalence}.`)

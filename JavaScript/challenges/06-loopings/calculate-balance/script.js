76ef4a219a70fd362cac47500d6dc5a16b3e9622
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-23 10:37:29 -0300

    add challenge calculate balance

--- JavaScript/challenges/06-loopings/calculate-balance/script.js
+++ JavaScript/challenges/06-loopings/calculate-balance/script.js
@@ -1,50 +1,42 @@
-// Crie dois objetos do tipo Arrey com Receita e Despesas. 
-// Depois uma função para calcular o saldo
-// Diga se o saldo está Positivo ou Negativo.
+// Crie uma função para calcular o saldo com dois argumentos: Receita e Despesas
+// Imprima o saldo formatado, se está Positivo ou Negativo e se existe dívidas.
+// Use as duas listas abaixo. Dica: Trate os dados com os métodos do array
 
-let receita = [200, 2800.25, 800]
-let despesa = [500, 2500, 300.89]
+const incomes = [
+    'salary', 'R$ 2000,00',
+    'commission', 'R$ 825,45',
+    'bônus', 'R$ 320,00',
+]
 
-function saldo(receita, despesa){
+const expenses = [
+    'rent', 'R$ 500,00',
+    'energy', 'R$ 325,55',
+    'water', 'R$ 120,34',
+    'food', 'R$ 1300,62',
+]
 
-    let totalReceita = 0
-    for (let v of receita){ //Acessa o Valor
-    totalReceita += v
-    }
-    
-    let totalDespesa = 0
-    for(let i in despesa){ // Acessa o Índice
-    totalDespesa += despesa[i]
-    }
-    
-    return totalReceita - totalDespesa
+function balence(incomes, expenses) {
+    const incomeValues = incomes.filter((value) => value.includes('R$'))
+    const incomeValuesNumber = incomeValues.map((income) => Number(income.replace("R$ ", "").replace(',', '.')))
+    const incomesSum = incomeValuesNumber.reduce((a, b) => a + b)
+
+    const expensesValues = expenses.filter((value) => value.includes('R$'))
+    const expensesValuesNumber = expensesValues.map((expense) => Number(expense.replace("R$ ", "").replace(',', '.')))
+    const expensesSum = expensesValuesNumber.reduce((a, b) => a + b)
+
+    return incomesSum - expensesSum
 }
 
-totalReceita = 0
-for (let v of receita){ //Acessa o Valor
-    totalReceita += v
+let statusBalence
+let resultBalance = balence(incomes, expenses)
+
+if ( resultBalance > 0) {
+    statusBalence = 'Existe um superávit. Invista o dinheiro!'
+}
+else if ( resultBalance < 0) {
+    statusBalence = 'Foi gasto todo dinheiro e existe dívidas!'
+} else {
+    statusBalence = 'Foi gasto todo dinheiro, mas não existe dívidas!'
 }
 
-totalDespesa = 0
-for(let i in despesa){ // Acessa o Índice
-    totalDespesa += despesa[i]
-}
-
-console.log(`Receita: ${totalReceita.toLocaleString('pt-br', { style: 'currency', currency: 'brl'})}`)
-
-console.log(`Despeza: ${totalDespesa.toLocaleString('pt-br', {style: 'currency', currency: 'brl'})}`)
-
-saldoFinal =saldo(receita, despesa)
-console.log(`Total: ${saldoFinal.toLocaleString('pt-br', {style:'currency', currency: 'brl'})}`)
-
-
-
-if (saldoFinal > 0){
-    console.log('Saldo Positivo!')
-}
-else if (saldoFinal == 0){
-    console.log('Sem Saldo!')
-}
-else{
-    console.log('Saldo Negativo!')
-}
+console.log(`$Saldo de ${resultBalance.toLocaleString('pt-br', {style: 'currency', currency: 'brl'})}. ${statusBalence}`)

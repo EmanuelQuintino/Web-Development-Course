b0ca7124da1ca658e340818f08634349195a3732
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-25 16:53:07 -0300

    add challenge array methods

--- JavaScript/challenges/8-dom/Infinite-Autoplay-Carousel/script.js
+++ JavaScript/challenges/8-dom/Infinite-Autoplay-Carousel/script.js
@@ -3,17 +3,16 @@ const $nextButton = document.querySelector(".nextButton img")
 const $previousButton = document.querySelector(".previousButton img")
 
 
-
 function nextImage() {
     let firstCarouselImage = document.querySelectorAll(".carouselImages img")[0]
 
     $carouselImages.style.marginLeft = "-200%"
     $carouselImages.style.transition = "margin-left 1s"
-    
+
     setTimeout(() => {
         $carouselImages.style.transition = "none"
         $carouselImages.insertAdjacentElement("beforeend", firstCarouselImage)
-        $carouselImages.style.marginLeft = "-100%"   
+        $carouselImages.style.marginLeft = "-100%"
     }, 1000);
 }
 
@@ -23,24 +22,23 @@ function previousImage() {
 
     $carouselImages.style.marginLeft = "0%"
     $carouselImages.style.transition = "margin-left 1s"
-    
+
     setTimeout(() => {
         $carouselImages.style.transition = "none"
         $carouselImages.insertAdjacentElement("afterbegin", lastCarouselImage)
-        $carouselImages.style.marginLeft = "-100%"   
+        $carouselImages.style.marginLeft = "-100%"
     }, 1000);
 }
-
 
 
 let intervalNextImage = setInterval(nextImage, 2000)
 
 $nextButton.addEventListener("click", () => {
-    clearInterval(intervalNextImage) 
+    clearInterval(intervalNextImage)
     nextImage()
 })
 $previousButton.addEventListener("click", () => {
-    clearInterval(intervalNextImage) 
+    clearInterval(intervalNextImage)
     previousImage()
 })
 

f6643cd9175fdc95396a7cc676801f7f2a3a5ed0
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-25 13:21:22 -0300

    add class local storage

--- JavaScript/class-06-loopings/script.js
+++ JavaScript/class-06-loopings/script.js
@@ -17,9 +17,10 @@
 //     count++ // container
 // }
 
+// let resp = false
 // do {
-    // console.log("Do While")
-// } while (count = 0);
+//     resp = prompt('Deseja sair?')
+// } while (resp)
 
 // console.log("Running...")
 
@@ -39,16 +40,22 @@
 //         }
 // }
 
+// const array = [12, "text", () => "A"]
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+//     console.log(element)
+// }
+
 
 
 // FOR (OF, IN)
 
-// let team = 'Brasil'
-// let team = {name: 'Brasil', cups: 5}
-// let teams = ['Brasil', 'Argentina', 'Portugal', 'Alemanha']
+// const team = 'Brasil'
+// const team = {name: 'Brasil', cups: 5}
+// const teams = ['Brasil', 'Argentina', 'Portugal', 'Alemanha']
 
 
-// for(let value of teams){
+// for(let value of team){
 //     console.log(value)
 // }
 
@@ -60,39 +67,54 @@
 
 
 
-// ARRAYS (Methods)
+// ARRAYS (Methods) - Principle of Immutability
 
 // teams.forEach((value, index, array) => {
     // console.log(value, index, array)
 // })
 
-// let team = Array.from('Brasil')
-// team.forEach((team, index, array) => {
+let team = Array.from('Brasil')
+// let testReturnForEach = team.forEach((team, index, array) => {
 //     console.log(team, index, array)
 // })
 
+// console.log(testReturnForEach) // no return
+
+// let testReturnMap = team.map((team, index, array) => {
+//     console.log(team, index, array)
+//     return [team, index, array]
+// })
+
+// console.log(testReturnMap) // return
+
 const real = [10, 20, 30, 40, 50]
 // const convertDollar = real.map((value) => value * 5.36)
+// const sumReal = real.reduce((a, b) => a + b)
+
 // const sumConvertedDollar = convertDollar.reduce((a, b) => a + b)
 
 // console.log(convertDollar)
 // console.log(sumConvertedDollar)
+
 // console.log(real)
+// console.log(sumReal)
 
 // console.log(real.map(value => value * 5.36).reduce((a, b) => a + b))
 
-console.log(real.filter((value) => value > 30))
+// console.log(real.filter((value) => value > 30))
 
-const avaregeStudants = [
-    {name: 'João', avarege: 8},
-    {name: 'Thiago', avarege: 8},
-    {name: 'Pedro', avarege: 7},
-    {name: 'Jonas', avarege: 5},
-    {name: 'Rosa', avarege: 9},
-]
+// const avaregeStudants = [
+//     {name: 'João', avarege: 2},
+//     {name: 'Thiago', avarege: 4},
+//     {name: 'Pedro', avarege: 7},
+//     {name: 'Jonas', avarege: 5},
+//     {name: 'Rosa', avarege: 9},
+// ]
 
-const approvedStudants = avaregeStudants.filter((value) => {
-    if (value.avarege >= 7) {
-        console.log(value.name)
-    }
-})
+// let approvedStudants = avaregeStudants.filter((value) => {
+//     if (value.avarege >= 7) {
+//         return console.log(value.name)
+//     }
+// })
+
+// console.log(approvedStudants)

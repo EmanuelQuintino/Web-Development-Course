fe5aaa18a576650b23d74ea3e62ec4ac75e92d66
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-29 09:03:25 -0300

    add challenge bingo

---
+++ JavaScript/challenges/8-dom/bingo/script.js
@@ -0,0 +1,82 @@
+const flipBackWord = document.querySelector(".flipBack span")
+const wordDrawn = document.querySelector(".wordDrawn span")
+const buttonDrawn = document.querySelector(".buttonDrawn")
+const buttonNewBingo = document.querySelector(".buttonNewBingo")
+const tipWordDrawn = document.querySelector(".tipWordDrawnContainer span")
+const listWordsDrawnContainer = document.querySelector(".listWordsDrawnContainer")
+
+const wordList = [
+    {number: 1, word: "Neymar", tips: "Nome do camisa 10 da seleção brasileira"},
+    {number: 2, word: "Catar", tips: "País cede da copa do mundo de 2022"},
+    {number: 3, word: "França", tips: "Vencedor da copa de 2018"},
+    {number: 4, word: "2002", tips: "Ano que o Brasil ganhou a quinta copa"},
+    {number: 5, word: "Alemanha", tips: "País que o Brasil venceu na final de 2002"},
+]
+
+function showWordsDrawn() {
+    listWordsDrawnContainer.innerHTML = `${wordList.map((value) => {
+        let wordWasDrawn = ''
+        for (let index = 0; index <= localStorage.length; index++) {
+            if (value.word == localStorage.getItem(index + 1)) {
+                wordWasDrawn = "class='wordWasDrawn'"
+            }
+        }
+        return `<div ${wordWasDrawn}>${value.number}-${value.word}</div>`
+    }).join('')}`
+}
+
+function newNumberDrawn() {
+    const numbersDrawn = []
+    for (let index = 0; index < localStorage.length; index++) {
+        numbersDrawn.push(localStorage.getItem(index + 1))
+    }
+
+    while(wordList.length != localStorage.length) {
+        let numDrawn = Math.ceil(Math.random() * wordList.length)
+
+        if (!numbersDrawn.includes(wordList[numDrawn - 1].word)) {
+            return numDrawn
+        }
+    }
+}
+
+function bingoDrawn(numDrawn) {
+    if (numDrawn) {
+        count = localStorage.length
+        localStorage.setItem(count + 1, wordList[numDrawn - 1].word)
+        count++
+
+        tipWordDrawn.innerHTML = wordList[numDrawn - 1].tips
+        wordDrawn.innerHTML = wordList[numDrawn - 1].word
+    }
+    else {
+        window.alert('Bingo encerrado!')
+    }
+}
+
+function newBingo() {
+    let resp = window.confirm('Deseja iniciar o Bingo?')
+    if(resp) {
+        localStorage.clear()
+        showWordsDrawn()
+    }
+}
+
+
+buttonDrawn.addEventListener('click', () => {
+    bingoDrawn(newNumberDrawn())
+})
+
+flipBackWord.addEventListener('click', () => {
+    showWordsDrawn()
+})
+
+buttonNewBingo.addEventListener('click', () => {
+    newBingo()
+})
+
+listWordsDrawnContainer.addEventListener('click', ()=> {
+    showWordsDrawn()
+})
+
+showWordsDrawn()

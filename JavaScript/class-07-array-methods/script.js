720ac03efc501715596a7726710e4f4f3ac33993
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-30 17:19:53 -0300

    update array class methods

--- JavaScript/class-07-array-methods/script.js
+++ JavaScript/class-07-array-methods/script.js
@@ -4,11 +4,12 @@ const team = 'Brasil'
 // const team = {name: 'Brasil', cups: 5}
 const teams = ['Brasil', 'Argentina', 'Portugal', 'Alemanha']
 
+
 // teams.forEach((value, index, array) => {
 //     console.log(value, index, array)
 // })
 
-let teamArray = Array.from(team)
+// let teamArray = Array.from(team)
 // let testReturnForEach = teamArray.forEach((value, index, array) => {
 //     console.log(value, index, array)
 // })
@@ -51,30 +52,26 @@ const averageStudants = [
     {id: 6, name: 'Pedro', average: 7, birth: 1993},
 ]
 
-let approvedStudants = averageStudants.filter((studant) => studant.average >= 7)
+// console.table(averageStudants.filter((studant) => studant.average >= 7));
+// console.table(averageStudants.filter(studant => studant.birth >= 2000))
 
-let birthStudants = averageStudants.filter((studant) => studant.birth >= 2000)
+let yearCurrent = new Date().getFullYear()
+// console.log(yearCurrent);
 
-let ageStudants = averageStudants.filter((studant) => (new Date().getFullYear() - studant.birth) >= 30)
+// console.table(averageStudants.filter(studant => (yearCurrent - studant.birth) >= 30))
+// console.table(averageStudants.filter((studant) => Array.from(studant.name)[0].toLocaleLowerCase() == "j"));
 
-// console.table(approvedStudants)
-// console.table(birthStudants)
-// console.table(ageStudants)
-
-// let findStudant = averageStudants.find((studant) => (new Date().getFullYear() - studant.birth) >= 30)
-
-// let findStudant = averageStudants.find((studant) => studant.name.toLocaleLowerCase() == 'amanda')
-
-// let findStudant = averageStudants.find((studant) => studant.id == 5)
-
-// console.log(findStudant)
-
+// console.log(averageStudants.find((studant) => (yearCurrent - studant.birth) >= 30));
+// console.log(averageStudants.find((studant) => studant.name.toLocaleLowerCase() == 'amanda'));
+// console.log(averageStudants.find((studant) => studant.id == 5));
 
 // Table ASCII (0-9, A-Z, a-z)
-console.table(averageStudants.sort((a, b) => b.average - a.average))
+// console.table(averageStudants.sort((a, b) => b.average - a.average))
 
 console.table(averageStudants.sort((a, b) => {
     if (a.name > b.name) return 1;
     if (a.name < b.name) return -1;
     return 0;
 }))
+
+// console.log(teams.concat(team))

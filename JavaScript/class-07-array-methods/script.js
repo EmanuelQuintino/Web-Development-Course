eef701d64fbad0ba79f027c6439a48ffaad43710
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-30 12:05:49 -0300

    update class array methods

--- JavaScript/class-07-array-methods/script.js
+++ JavaScript/class-07-array-methods/script.js
@@ -24,33 +24,35 @@ let teamArray = Array.from(team)
 
 
 const real = [10, 20, 30, 40, 50]
-// const convertDollar = real.map((value) => value * 5.36)
 
-let initial = 200
-const sumReal = real.reduce((a, b) => a + b, initial)
+// let initial = 200
+// const sumReal = real.reduce((a, b) => a + b, initial)
+
+// console.log(real)
+// console.log(sumReal)
+
+// const convertDollar = real.map((value) => value * 5.36)
 // const sumConvertedDollar = convertDollar.reduce((a, b) => a + b)
 
 // console.log(convertDollar)
 // console.log(sumConvertedDollar)
 
-// console.log(real)
-// console.log(sumReal)
-
 // console.log(real.map(value => value * 5.36).reduce((a, b) => a + b))
 
-// console.log(real.filter((value) => value < 30))
 
+// console.log(real.filter((value) => value < 40))
 
 const averageStudants = [
-    {id: 1, name: 'João', average: 2, birth: 1995},
-    {id: 2, name: 'Thiago', average: 4, birth: 2002},
-    {id: 3, name: 'Jonas', average: 5, birth: 1988},
+    {id: 1, name: 'João', average: 4, birth: 1990},
+    {id: 2, name: 'Thiago', average: 6, birth: 2002},
+    {id: 3, name: 'Jonas', average: 5, birth: 1998},
     {id: 4, name: 'Rosa', average: 9, birth: 2004},
     {id: 5, name: 'Amanda', average: 8, birth: 1992},
-    {id: 6, name: 'Pedro', average: 7, birth: 1990},
+    {id: 6, name: 'Pedro', average: 7, birth: 1993},
 ]
 
 let approvedStudants = averageStudants.filter((studant) => studant.average >= 7)
+
 let birthStudants = averageStudants.filter((studant) => studant.birth >= 2000)
 
 let ageStudants = averageStudants.filter((studant) => (new Date().getFullYear() - studant.birth) >= 30)
@@ -61,15 +63,15 @@ let ageStudants = averageStudants.filter((studant) => (new Date().getFullYear() 
 
 // let findStudant = averageStudants.find((studant) => (new Date().getFullYear() - studant.birth) >= 30)
 
-let findStudant = averageStudants.find((studant) => studant.name.toLocaleLowerCase() == 'thiago')
+// let findStudant = averageStudants.find((studant) => studant.name.toLocaleLowerCase() == 'amanda')
 
-// let findStudant = averageStudants.find((studant) => studant.id == 2)
+// let findStudant = averageStudants.find((studant) => studant.id == 5)
 
 // console.log(findStudant)
 
 
 // Table ASCII (0-9, A-Z, a-z)
-// console.table(averageStudants.sort((a, b) => a.birth - b.birth))
+console.table(averageStudants.sort((a, b) => b.average - a.average))
 
 console.table(averageStudants.sort((a, b) => {
     if (a.name > b.name) return 1;

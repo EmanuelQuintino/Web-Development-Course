c74fdc88cf135d8354eed5f9666416e1c7b0d00d
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-11-30 19:20:41 -0300

    refactor code mega sena draw

--- JavaScript/challenges/06-loopings/mega-sena-draw/script.js
+++ JavaScript/challenges/06-loopings/mega-sena-draw/script.js
@@ -1,6 +1,5 @@
 // Crie um algoritmo que sortei os números da Mega Sena
-// São 6 números aleatórios não duplicados de 1 a 60  
-// Use um Arrey para guardar os números e apresentá-los
+// São 6 números aleatórios não duplicados de 1 a 60
 // Os números devem estar em ordem crescente, separandos com "-"
 // Insira 0 a esquerda para os números menores de 10. EX: 03-09.
 
@@ -8,16 +7,14 @@ const numbersMega = []
 
 while(numbersMega.length < 6 ) {
     let num = Math.ceil(Math.random() * 60)
-    
+
     if(!numbersMega.includes(num)){
         numbersMega.push(num)
     }
-}    
-
-numbersMega.sort((a, b) => a - b)
-
-let count = 0
-for (const num of numbersMega) {
-    document.write(`${count < 5 ? String(num).padStart(2, "0") + "-" : String(num).padStart(2, "0")}`)
-    count += 1
 }
+
+let showNumbers = numbersMega.map((number) => {
+    return String(number).padStart(2, 0)
+}).sort((a, b) => a - b).join('-')
+
+document.write(showNumbers)

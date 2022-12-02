d7bfe8cb8ef64a6ba7678ea2faca3ee9ea78cc81
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-12-01 21:00:55 -0300

    add class looping array multidimensional

--- JavaScript/class-06-loopings/script.js
+++ JavaScript/class-06-loopings/script.js
@@ -25,19 +25,18 @@
 // console.log("Running...")
 
 
-
 // FOR
 
 // for (let count = 0; count <= 10; count++) { // increment factor
-//         if (count > 2 && count < 8) {
-//             count ++ // Caution, two increments!
-//             continue
-//             // break
-//         }
+//     if (count > 2 && count < 8) {
+//         count ++ // Caution, two increments!
+//         continue
+//         // break
+//     }
 
-//         if (count % 2 == 0) {
-//             console.log(count)
-//         }
+//     if (count % 2 == 0) {
+//         console.log(count)
+//     }
 // }
 
 // const array = [12, "text", () => "A"]
@@ -46,21 +45,51 @@
 //     console.log(element)
 // }
 
+// const array = [[1, 2, 3], ['a', 'b', 'c'], [[], {}, ()=>'X']] // multidimensional
+
+// console.log(array[0][1]);
+// console.log(array[1][2]);
+// console.log(array[2][2]());
+
+// for (let i = 0; i < array.length; i++) {
+//     for (let j = 0; j < array[i].length; j++) {
+//         console.log(array[i][j])
+//     }
+// }
 
 
 // FOR (OF, IN)
 
 // const team = 'Brasil'
-// const team = {name: 'Brasil', cups: 5}
-// const teams = ['Brasil', 'Argentina', 'Portugal', 'Alemanha']
+const team = {name: 'Brasil', cups: 5}
 
-
-// for(let value of team){
+// for(let value of team){ // Not iterable {}
 //     console.log(value)
 // }
 
 // for(let index in team){
+//     console.log(index)
 //     console.log(team[index])
 // }
 
 // console.log(team)
+
+const teams = ['Brasil', 'Argentina', 'Portugal', 'Alemanha']
+
+// for(let value of teams){
+//     console.log(value)
+// }
+
+// for(let index in teams){
+//     console.log(teams[index])
+// }
+
+// console.log(teams)
+
+const array = [[1, 2, 3], ['a', 'b', 'c'], [[], {}, ()=>'X']]
+
+// for (const i of array) {
+//     for (const j of array[i]) { // Not iterable array[i]
+//         console.log(array[i][j])
+//     }
+// }

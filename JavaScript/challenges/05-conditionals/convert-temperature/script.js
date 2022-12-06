44e3ed6a9363e0b2a57be042c6d3edd3e562f5be
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-12-06 16:19:23 -0300

    update challenge text

--- JavaScript/challenges/05-conditionals/convert-temperature/script.js
+++ JavaScript/challenges/05-conditionals/convert-temperature/script.js
@@ -1,6 +1,6 @@
 // Crie um programa que converta a temperatura Celsius ou Fahrenheit
-// O algoritmo deve ser capaz de interpretar o valor passado e converter para o outro
-// A temperatura de entrada e de saída deve conter a letra de referência (Ex: 10C = 50F)
+// A temperatura de entrada e de saída deve conter a letra de referência ('C' ou 'F')
+// O algoritmo deve ser capaz de interpretar o valor passado e converter para o outro (Ex: 10C = 50F)
 // Deve funcionar com espaços, letras minúsculas e tratar dados inválidos (Throw, Try e Catch).
 
 // C = (F - 32) * 5/9

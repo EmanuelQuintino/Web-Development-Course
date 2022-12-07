f7c54e0f594c2b1bcac4ddd157ffdbe42158c1fe
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-12-07 13:18:31 -0300

    add category validations

--- JavaScript/challenges/06-loopings/list-films/script.js
+++ JavaScript/challenges/06-loopings/list-films/script.js
@@ -1,7 +1,7 @@
 // Crie um sistema que receba listas de filmes por categoria
 // Apresente o total das categorias e dos filmes, depois sugira as listas que deseja ver
-// A sugestão deve persistir enquanto o usuário não colocar um nome válido
-// Mostre o total de filmes daquela categoria e os nomes de cada filme
+// A sugestão deve persistir enquanto o usuário não colocar um nome válido (faça validações)
+// Mostre a categoria, quantos filmes ela tem e os nomes de cada um deles.
 
 const listFilms = [
     {category: 'Ação', films: ['Jurassic Park', 'Transformers']},
@@ -25,17 +25,32 @@ let category
 while (!categories.includes(category)) {
     category = window.prompt(`São ${totalCategories} categorias com ${totalFilms} filmes
 Qual lista deseja ver? (${categories.join(", ")})`)
+
+    if (category == "") {
+        alert("Insira o nome de uma das categorias.")
+        continue;
+    }
+
+    if (category == null) {
+        alert("Programa encerrado!");
+        break;
+    }
+
+    category = category.toLowerCase().split(" ").join("")
+    category = category[0].toUpperCase() + category.substring(1)
 }
 
-let count = 0
-const films = []
-for(let value of listFilms){
-    if(value.category == category){
-        for(let film of value.films){
-            count += 1
-            films.push(film)
+if (category != null) {
+    let count = 0
+    const films = []
+    for(let value of listFilms){
+        if(value.category == category){
+            for(let film of value.films){
+                count += 1
+                films.push(film)
+            }
         }
     }
-}
 
-window.alert(`Total de ${count} filme(s): ${films.join(", ")}`)
+    window.alert(`Total de ${count} filme(s) de ${category}: ${films.join(", ")}`)
+}

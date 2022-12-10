59e771b6eca3b3ac5cd17628bc6b4f267fb548e1
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-12-10 20:47:50 -0300

    add class es6 modules

---
+++ JavaScript/class-09-es6-modules/scripts/functions.js
@@ -0,0 +1,29 @@
+// export function sum(num1, num2) {
+//     return num1 + num2
+// }
+
+// ------------------------------------------
+
+function sum(num1, num2) {
+    return num1 + num2
+}
+
+function sub(num1, num2) {
+    return num1 - num2
+}
+
+function mul(num1, num2) {
+    return num1 * num2
+}
+
+function div(num1, num2) {
+    return num1 / num2
+}
+
+export { sum, sub, mul, div }
+export const operations = ({
+    sum,
+    sub,
+    mul,
+    div
+})

8e82a631175235cc319e2272c68851c5fe914bc8
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-12-19 19:15:13 -0300

    remove calculate button event touchstart

--- Projects/imc-calculator/script.js
+++ Projects/imc-calculator/script.js
@@ -44,8 +44,8 @@ function imcCalculate(event) {
     }
 }
 
-calculateButton.addEventListener("touchstart", imcCalculate)
 calculateButton.addEventListener("click", imcCalculate)
+// calculateButton.addEventListener("touchstart", imcCalculate)
 
 closeButton.addEventListener("click", () => {
     modalClose()

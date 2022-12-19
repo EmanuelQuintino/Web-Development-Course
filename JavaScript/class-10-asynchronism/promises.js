d4b00a99fa10aca89dc975c1be2979f8ed32a49f
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-12-19 13:21:44 -0300

    update class promises

--- JavaScript/class-10-asynchronism/promises.js
+++ JavaScript/class-10-asynchronism/promises.js
@@ -1,34 +1,37 @@
-// console.log('Fazer requisição');
+console.log('Fazer requisição');
 
-// const promise = new Promise((resolve, reject) => {
-//     let situation = false
-//     if (situation) {
-//         return resolve('Resposta do Banco de Dados!')
-//     } else {
-//         return reject('Sem resposta!')
-//     }
-// })
+const promise = new Promise((resolve, reject) => {
+    let situation = true
+    if (situation) {
+        return resolve('Resposta do Banco de Dados!')
+    } else {
+        return reject('Sem resposta!')
+    }
+})
 
-// promise
-//     .then((answer) => console.log(answer))
-//     .catch((error) => console.log(error))
-//     .finally(() => console.log('Requisição concluída!'))
+promise
+    .then((answer) => console.log(answer))
+    .catch((error) => console.log(error))
+    .finally(() => console.log('Requisição concluída!'))
 
-// console.log('Finalizar requisição');
+async function requestData() {
+    try {
+        const response = await promise
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    } finally {
+        console.log('Requisição concluída!');
+    }
+}
 
-const request = fetch('https://api.github.com/users/EmanuelQuintino')
+requestData()
+console.log('Finalizar requisição');
+
+const url = 'https://api.github.com/users/EmanuelQuintino'
+const requestUser = fetch(url)
     .then(request => request.json())
     .then(json => fetch(json.repos_url))
     .then(response => response.json())
     .then(data => console.log(data))
     .catch((error) => console.log(error))
-
-
-// async function objectFunc(url) {
-//     const request = await fetch(url)
-//     const json = await request.json()
-//     return json
-
-// }
-
-// console.log(objectFunc(url));

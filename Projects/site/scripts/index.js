489d1794e45fc49a2976a506ee35ca310434c750
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-12-19 17:50:32 -0300

    update project site

--- Projects/site/scripts/index.js
+++ Projects/site/scripts/index.js
@@ -1,6 +1,10 @@
 const myHeader = document.querySelector("header");
 let lastScrollY = window.scrollY;
 
+function submitMessage() {
+    alert("Formulário submetido com sucesso!")
+}
+
 window.addEventListener("scroll", () => {
     if(lastScrollY < window.scrollY && window.scrollY >= 100) {
         console.log("Down");

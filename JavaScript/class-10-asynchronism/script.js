5171e244df138bace28737377def1c5aa879e838
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-12-19 23:47:15 -0300

    update class promises fetch api github

---
+++ JavaScript/class-10-asynchronism/script.js
@@ -0,0 +1,93 @@
+// >>>>>>> Asynchronism <<<<<<<
+
+// function print(num) {
+//     console.log(num);
+// }
+
+// function sum(num1, num2, callback) {
+//     let sum = num1 + num2
+//     callback(sum)
+// }
+
+// sum(2, 3, print)
+
+// >>>>>>>> setTimeout <<<<<<<<<
+
+// console.log('Antes');
+// let timeOut = setTimeout(() => {
+//     console.log('Test TimeOut ms');
+// }, 2000)
+// console.log('Depois');
+
+// let index = 0
+// const array = [12, 28, 33, 46, 52, 59]
+// const newArray = []
+// let interval = setInterval(() => {
+//     newArray.push(array[index])
+
+//     let numbers = document.querySelector('.numbers')
+//     numbers.innerHTML = newArray.join('-')
+
+//     if (array.length == newArray.length) {
+//         clearInterval(interval)
+//     }
+
+//     index++
+// }, 1500)
+
+// // console.log(timeOut);
+// // console.log(interval);
+
+// let isFinish = false
+// if (isFinish) {
+//     clearInterval(interval)
+// }
+
+
+// >>>>>>>>>> Promises <<<<<<<<<<<<<<
+
+// console.log('Fazer requisição');
+
+// const promise = new Promise((resolve, reject) => {
+//     let situation = true
+//     if (situation) {
+//         return resolve('Resposta do Banco de Dados!')
+//     } else {
+//         return reject('Sem resposta!')
+//     }
+// })
+
+// promise
+//     .then((answer) => console.log(answer))
+//     .catch((error) => console.log(error))
+//     .finally(() => console.log('Requisição concluída!'))
+
+// async function requestData() {
+//     try {
+//         const response = await promise
+//         console.log(response);
+//     } catch (error) {
+//         console.log(error);
+//     } finally {
+//         console.log('Requisição concluída!');
+//     }
+// }
+
+// requestData()
+// console.log('Finalizar requisição');
+
+const userName = 'EmanuelQuintino'
+const url = `https://api.github.com/users/${userName}`
+const gitHubUser = fetch(url)
+    .then(request => request.json())
+    .then(({ name, bio, public_repos }) => ({
+            name,
+            bio,
+            public_repos
+        })
+    )
+    .catch((error) => console.log(error))
+
+gitHubUser.then((user) => {
+  console.log(`${user.name} tem ${user.public_repos} repositório(s) públicos: ${user.bio}`);
+})

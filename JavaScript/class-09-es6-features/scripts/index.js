17a56d3c8290d4745153ce94726757f44ea97d21
Emanuel Quintino <emanuelquintino@hotmail.com>
2022-12-22 10:20:59 -0300

    update class es6 features text

--- JavaScript/class-09-es6-features/scripts/index.js
+++ JavaScript/class-09-es6-features/scripts/index.js
@@ -56,33 +56,35 @@ const person2 = person('João', 17)
 
 
 //destructure
-const array = ['A', 'B', 'C', 'D']
+const array = ['A', 'B']
 const [item1, item2] = array
 // console.log(item1)
 // console.log(item2)
 
 
 // rest
-function showItens(array, ...itens) {
-    console.log(array);
-    console.log(itens);
+function showArray(array, ...itens) {
+    // console.log(array);
+    // console.log(itens);
 }
 
-// showItens(array, 'E', 'F', 'G')
+showArray(array, 'D', 'E', 'F')
 
 
 // spread
 const numbers = [1, 3, 42, 5, 87, 6]
+// console.log(...numbers)
 // console.log(Math.max(...numbers))
 
-const clone = numbers
+// const clone = numbers
 // const clone = [...numbers]
+// const [...clone] = numbers
 // clone.pop()
 // console.log(clone);
 // console.log(numbers);
 
 const listItens = document.querySelectorAll("div");
-// console.log(listItens);
+console.log(listItens);
 // listItens.forEach(value => console.log(value))
 
 // let arrayListItens = Array.from(listItens)
@@ -93,7 +95,8 @@ const listItens = document.querySelectorAll("div");
 // console.log(arrayListItens);
 // arrayListItens.map(value => console.log(value));
 
-// [...listItens].map(value => console.log(value))
+// [...listItens].map(value => console.log(value));
+// [...numbers].map(value => console.log(value));
 
 // let number; number = 7
 // console.log(number);

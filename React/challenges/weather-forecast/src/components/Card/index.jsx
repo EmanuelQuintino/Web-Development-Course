e3f37fb2914a4943852e9f4d789d099f380ecc08
EmanuelQuintino <emanuelquintino@hotmail.com>
2022-12-29 20:53:36 -0300

    add css component card

--- React/challenges/weather-forecast/src/components/Card/index.jsx
+++ React/challenges/weather-forecast/src/components/Card/index.jsx
@@ -5,21 +5,48 @@ export function Card() {
         <article>
             <section>
                 <h2>Jucás</h2>
-                <p>Parcialmente nublado</p>
-                <span>Vento 10km/h</span>
+                <div>
+                    <p>Parcialmente nublado</p>
+                    <span>Vento 10km/h</span>
+                </div>
             </section>
             <section className="temperature">
                 <img src="" alt="imagem" />
                 <p>23°</p>
             </section>
-            <section className="dayForecast">
-                <div className="temperatureForecast">
-                    <img src="" alt="image"/>
-                    25°
+            <section className="daysForecast">
+                <div className="dayForecast">
+                    <h3>Segunda</h3>
+                    <div className="temperatureForecast">
+                        <img src="" alt="image"/>
+                        25°
+                    </div>
+                    <div className="windForecast">
+                        <img src="" alt="image"/>
+                        12km/h
+                    </div>
                 </div>
-                <div className="windForecast">
-                    <img src="" alt="image"/>
-                    12km/h
+                <div className="dayForecast">
+                    <h3>Segunda</h3>
+                    <div className="temperatureForecast">
+                        <img src="" alt="image"/>
+                        25°
+                    </div>
+                    <div className="windForecast">
+                        <img src="" alt="image"/>
+                        12km/h
+                    </div>
+                </div>
+                <div className="dayForecast">
+                    <h3>Segunda</h3>
+                    <div className="temperatureForecast">
+                        <img src="" alt="image"/>
+                        25°
+                    </div>
+                    <div className="windForecast">
+                        <img src="" alt="image"/>
+                        12km/h
+                    </div>
                 </div>
             </section>
         </article>

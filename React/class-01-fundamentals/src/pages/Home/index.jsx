f26c8c5e17ccb65931ae7619af61ac7e68759e84
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-01-01 11:58:02 -0300

    add exemple hook use state

--- React/class-01-fundamentals/src/pages/Home/index.jsx
+++ React/class-01-fundamentals/src/pages/Home/index.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 import "./style.css"
 
 export function Home() {
+  const [num, setNum] = useState(1)
+  // let num = 1
+  function increment() {
+    setNum(num + 1)
+    // num += 1
+    console.log(num);
+  }
   return (
     <React.Fragment>
       <header>
@@ -11,7 +18,8 @@ export function Home() {
       <main>
         <article>
           <section>
-            <h2>Paragraph 1</h2>
+            <h2>Paragraph {num}</h2>
+            <button onClick={increment}>Increment</button>
             <p>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, quis quod reiciendis sequi quasi ratione fuga tempora nostrum quisquam ipsum quibusdam asperiores! Neque aliquam adipisci necessitatibus blanditiis esse eum nulla.
             </p>

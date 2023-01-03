1d46abeb132d524ff99ccb7525247e8a152c2bee
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-01-03 12:13:30 -0300

    add weather api link

--- Projects/weather-forecast/src/pages/Home/index.jsx
+++ Projects/weather-forecast/src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import "./style.css"
 
 export function Home() {
   return (
+    // https://api.weatherapi.com/v1/forecast.json?key=fb85b303e1fe4286a2b15407223112&q=jucas&days=5&lang=pt
     <h1>Weather Forecast</h1>
   )
 }

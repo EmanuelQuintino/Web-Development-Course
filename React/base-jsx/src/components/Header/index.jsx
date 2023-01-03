1fec59aee9930e34c55a79d715b8740beb297e16
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-01-03 20:13:00 -0300

    add component header

---
+++ React/base-jsx/src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+import "./style.css"
+
+export function Header() {
+    return (
+        <footer>
+            &copy; Web Developement Course - Jucás
+        </footer>
+    )
+}
--- React/base-jsx/src/main.jsx
+++ React/base-jsx/src/main.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Home } from './pages/Home'
 import { Router } from './pages/Router'
+import { Header } from './components/Header'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Home /> */}
     <Router />
+    <Header />
   </React.StrictMode>,
 )

810ea75e2799526b69836f09455fb38e5bbe7a07
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-01-03 13:37:06 -0300

    add react base jsx

---
+++ React/challenges/base-jsx/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+export function Home() {
+
+  return (
+    <div>
+      <h1>Class React Base JSX</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti pariatur officiis, similique necessitatibus repudiandae, eligendi dolor molestias nobis voluptatibus neque, nemo possimus tenetur voluptatem? Laudantium deleniti dolore illo harum fugit?
+      </p>
+    </div>    
+  )
+}

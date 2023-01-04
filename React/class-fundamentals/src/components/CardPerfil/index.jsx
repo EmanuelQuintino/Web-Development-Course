05bb39dc7d04bb08074534e1411db1bcf45414b6
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-01-04 12:44:15 -0300

    add class component exemple

--- React/class-fundamentals/src/components/CardPerfil/index.jsx
+++ React/class-fundamentals/src/components/CardPerfil/index.jsx
@@ -1,11 +1,24 @@
+import React from 'react'
 import './style.css'
 
-export function CardPerfile({login, name, age}) {
-    return (
-        <div className="cardContainer">
-            <img src={`https://github.com/${login}.png`} alt="image" />
-            <p>Nome: {name}</p>
-            <p>Idade: {age}</p>
-        </div>
-    )
+// export function CardPerfile({login, name, age}) {
+//     return (
+//         <div className="cardContainer">
+//             <img src={`https://github.com/${login}.png`} alt="image" />
+//             <p>Nome: {name}</p>
+//             <p>Idade: {age}</p>
+//         </div>
+//     )
+// }
+
+export class CardPerfile extends React.Component {
+    render() {
+        return (
+            <div className="cardContainer">
+                 <img src={`https://github.com/${this.props.login}.png`} alt="image" />
+                 <p>Nome: {this.props.name}</p>
+                 <p>Idade: {this.props.age}</p>
+             </div>
+        )
+    }
 }

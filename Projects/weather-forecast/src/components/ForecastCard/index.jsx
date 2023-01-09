7917a7c1ee442d3a8ecca30fdfb82b24787fc679
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-01-09 01:51:09 -0300

    add spinner animation

---
+++ Projects/weather-forecast/src/components/ForecastCard/index.jsx
@@ -0,0 +1,14 @@
+import "./style.css"
+
+export function ForecastCard({ day, icon, tempMax, tempMin }) {
+    return (
+        <div className="blockWeatherForecast">
+            <h3>{day}</h3>
+            <img src={icon} alt="icon" />
+            <p className="forecastDegreeMaxMin">
+                <span>{tempMax}°</span>
+                <span>{tempMin}°</span>
+            </p>
+        </div>
+    )
+}
--- Projects/weather-forecast/src/pages/App/index.jsx
+++ Projects/weather-forecast/src/pages/App/index.jsx
@@ -1,20 +1,30 @@
 import {useState, useEffect} from "react"
 import { GoSearch } from 'react-icons/go'
 import { BsThermometerHigh } from 'react-icons/bs'
+import { ImSpinner2 } from 'react-icons/im'
 import { ClimateCard } from "../../components/ClimateCard"
 import "./style.css"
+import { ForecastCard } from "../../components/ForecastCard"
 
 export function App() {  
   const [inputCity, setInputCity] = useState('')
   const [searchedCity, setSearchedCity] = useState('jucas')
   const [weatherData, setWeatherData] = useState(null)
-  const API = `https://api.weatherapi.com/v1/forecast.json?key=fb85b303e1fe4286a2b15407223112&q=${searchedCity}&days=5&lang=pt`
+  const [isLoading, setIsLoading] = useState(false)
+
+  const API = `https://api.weatherapi.com/v1/forecast.json?key=fb85b303e1fe4286a2b15407223112&q=${searchedCity}&days=4&lang=pt`
 
   async function getCityWeather() {
-    const response = await fetch(API)
-    const data = await response.json()
-    setWeatherData(data)
-    console.log(data.forecast.forecastday[0].day);
+    setIsLoading(true)
+    try {
+      const response = await fetch(API)
+      const data = await response.json()
+      setWeatherData(data)
+    } catch (error) {
+      alert(error);  
+    } finally {
+      setIsLoading(false)
+    }
   }
 
   function searchCity(event) {
@@ -31,113 +41,75 @@ export function App() {
       <header>
         <h1>Previsão do tempo</h1>
         <form action="">
-          <input type="text" onChange={(event) => setInputCity(event.target.value)}/>
+          <input 
+            type="text" 
+            onChange={(event) => setInputCity(event.target.value)}
+            placeholder='Nome da cidade'
+            />
           <GoSearch className='searchButton'/>
           <button onClick={searchCity}>Buscar</button>
         </form>
       </header>
 
-        <main>
-          {searchedCity && weatherData && (
-            <article>
-              <section className="blockCityName">
-                <h2>{weatherData.location.name}, {weatherData.location.region}</h2>
-                <p>{weatherData.location.country}</p>
-              </section>
+      <main>
+        { isLoading ? <ImSpinner2 className="spinner"/> 
+        :searchedCity && weatherData && (
+          <article>
+            <section className="blockCityName">
+              <h2>{weatherData.location.name}, {weatherData.location.region}</h2>
+              <p>{weatherData.location.country}</p>
+            </section>
 
-              <section className="blockCurrentTime">
-                <div className="currentTime">
-                  <div className="blockDegree">
-                    <BsThermometerHigh className='iconThermometer'/>
-                    <p className="degreeCurrent">{weatherData.current.temp_c}°</p>
-                    <p>
-                      <span className="degreeMax">{weatherData.forecast.forecastday[0].day.maxtemp_c}°</span>
-                      <span className="degreeMin">{weatherData.forecast.forecastday[0].day.mintemp_c}°</span>
-                    </p>
+            <section className="blockCurrentTime">
+              <div className="currentTime">
+                <div className="blockDegree">
+                  <BsThermometerHigh className='iconThermometer'/>
+                  <p className="degreeCurrent">{weatherData.current.temp_c}°</p>
+                  <p>
+                    <span className="degreeMax">{weatherData.forecast.forecastday[0].day.maxtemp_c}°</span>
+                    <span className="degreeMin">{weatherData.forecast.forecastday[0].day.mintemp_c}°</span>
+                  </p>
+                </div>
+                <div className="blockSituation">
+                  <img src={weatherData.current.condition.icon} alt="icon-weather" className="iconCurrentWeather"/>
+                  <div>
+                    <p>{weatherData.current.condition.text}</p>
+                    <p>Sensação térmica de {weatherData.current.feelslike_c}°</p>
                   </div>
-                  <div className="blockSituation">
-                    <img src={weatherData.current.condition.icon} alt="icon-weather" className="iconCurrentWeather"/>
-                    <div>
-                      <p>{weatherData.current.condition.text}</p>
-                      <p>Sensação térmica de {weatherData.current.feelslike_c}°</p>
-                    </div>
-                  </div>
                 </div>
-              </section>
+              </div>
+            </section>
 
-              <section className="containerWeatherCondition">
-                <ClimateCard climate='Vento' condition={`${weatherData.current.wind_kph}km/h`}/>
-                <ClimateCard climate='Umidade' condition={`${weatherData.current.humidity}%`}/>
-                <ClimateCard climate='Chuva' condition={`${weatherData.current.precip_mm}mm`}/>
-              </section>
+            <section className="containerWeatherCondition">
+              <ClimateCard climate='Vento' condition={`${weatherData.current.wind_kph}km/h`}/>
+              <ClimateCard climate='Umidade' condition={`${weatherData.current.humidity}%`}/>
+              <ClimateCard climate='Chuva' condition={`${weatherData.current.precip_mm}mm`}/>
+            </section>
 
-              <section className="containerWeatherForecast">
-                <div className="blockWeatherForecast">
-                  <h3>Seg</h3>
-                  <img src="" alt="icon" />
-                  <p className="forecastDegreeMaxMin">
-                      <span>28°</span>
-                      <span>23°</span>
-                  </p>
-                </div>
-
-                <div className="blockWeatherForecast">
-                  <h3>Ter</h3>
-                  <img src="" alt="icon" />
-                  <p className="forecastDegreeMaxMin">
-                      <span>28°</span>
-                      <span>23°</span>
-                  </p>
-                </div>
-
-                <div className="blockWeatherForecast">
-                  <h3>Qua</h3>
-                  <img src="" alt="icon" />
-                  <p className="forecastDegreeMaxMin">
-                      <span>28°</span>
-                      <span>23°</span>
-                  </p>
-                </div>
-
-                <div className="blockWeatherForecast">
-                  <h3>Qui</h3>
-                  <img src="" alt="icon" />
-                  <p className="forecastDegreeMaxMin">
-                      <span>28°</span>
-                      <span>23°</span>
-                  </p>
-                </div>
-
-                <div className="blockWeatherForecast">
-                  <h3>Sex</h3>
-                  <img src="" alt="icon" />
-                  <p className="forecastDegreeMaxMin">
-                      <span>28°</span>
-                      <span>23°</span>
-                  </p>
-                </div>
-
-                <div className="blockWeatherForecast">
-                  <h3>Sab</h3>
-                  <img src="" alt="icon" />
-                  <p className="forecastDegreeMaxMin">
-                      <span>28°</span>
-                      <span>23°</span>
-                  </p>
-                </div>
-
-                <div className="blockWeatherForecast">
-                  <h3>Dom</h3>
-                  <img src="" alt="icon" />
-                  <p className="forecastDegreeMaxMin">
-                      <span>28°</span>
-                      <span>23°</span>
-                  </p>
-                </div>
-              </section>
-            </article>
-          )}
-        </main>
+            <section className="containerWeatherForecast">
+              <ol>
+                {
+                  weatherData.forecast.forecastday.map((forecastday, index) => {
+                    return (
+                      <li  key={index}>
+                        <ForecastCard 
+                          day={index == 0 ? 'Hoje' 
+                          :Intl.DateTimeFormat('pt-BR', { weekday: 'short'})
+                          .format(new Date(forecastday.date))
+                          } 
+                          icon={forecastday.day.condition.icon} 
+                          tempMax={forecastday.day.maxtemp_c} 
+                          tempMin={forecastday.day.mintemp_c}
+                        />
+                      </li>
+                    )
+                  })
+                }
+              </ol>
+            </section>
+          </article>
+        )}
+      </main>
     </div>
     )   
 }

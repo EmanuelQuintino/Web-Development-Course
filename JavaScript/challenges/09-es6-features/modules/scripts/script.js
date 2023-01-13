d304da9dc46a08547b127044997f8a774d6cf60c
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-01-12 21:09:45 -0300

    update challenges location

--- JavaScript/challenges/09-es6-features/modules/scripts/script.js
+++
@@ -1,7 +0,0 @@
-// Crie uma pasta com 2 scripts dentro dela:
-// Um que execute uma função que imprima no log a mensagem 'Hello!'
-// O outro com a declaração dafunção.
-
-import { sayHello } from "./functions.js";
-
-sayHello()

f5880b6a39f72d9eed327e8df762c9d87a9cbd09
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-01-26 20:10:21 -0300

    add class 01 type commands

---
+++ 6.SQL/class-01-type-commands.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE school;
+DROP DATABASE school;
+
+USE school;
+
+CREATE TABLE studants(
+	named VARCHAR(50) NOT NULL,
+    age INT NOT NULL
+);
+
+DROP TABLE studants;
+
+SELECT * FROM studants;
+DESCRIBE studants;
+
+INSERT INTO studants(named, age) VALUES("Mario", 40);
+INSERT INTO studants VALUES('Emanuel', 30);
+INSERT INTO studants(age, named) VALUES(16, 'Murilo');
+
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE studants SET age = 30 WHERE named = 'Emanuel';
+DELETE FROM studants WHERE named = 'Emanuel';
+-- DELETE FROM studants;
+
+TRUNCATE studants;

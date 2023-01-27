c9da9681d4e2b1b356bf4ecb10eea8989b9fa282
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-01-27 13:03:14 -0300

    update class 01 type commands

--- 6.SQL/class-01-type-commands/type-commands.sql
+++ 6.SQL/class-01-type-commands/type-commands.sql
@@ -14,7 +14,7 @@ SELECT * FROM studants;
 DESCRIBE studants;
 
 INSERT INTO studants(named, age) VALUES("Samila", 18);
-INSERT INTO studants VALUES('Emanuel', 30);
+INSERT INTO studants VALUES('Emanuel', 30, DEFAULT);
 INSERT INTO studants(age, named) VALUES(16, 'Murilo');
 
 SET SQL_SAFE_UPDATES = 1;
@@ -24,3 +24,22 @@ DELETE FROM studants WHERE named = 'Emanuel';
 -- DELETE FROM studants;
 
 TRUNCATE studants;
+
+ALTER TABLE studants 
+ADD COLUMN city VARCHAR(20) NOT NULL DEFAULT 'Jucás'
+AFTER age;
+
+ALTER TABLE studants 
+ADD COLUMN id INT
+FIRST;
+
+ALTER TABLE studants DROP COLUMN id;
+
+ALTER TABLE studants 
+MODIFY COLUMN named VARCHAR(30) NOT NULL;
+
+ALTER TABLE estudantes RENAME TO studants;
+ALTER TABLE studants RENAME COLUMN named TO nome;
+
+ALTER TABLE studants 
+CHANGE COLUMN nome named VARCHAR(30) NOT NULL;

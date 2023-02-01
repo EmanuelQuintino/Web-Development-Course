05555be913acde6f35b83394f59513c87464f086
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-02-01 11:16:59 -0300

    add exemplo query params

--- 5.Node.JS/class-02-params/src/server.js
+++ 5.Node.JS/class-02-params/src/server.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3333;
 
+app.listen(port, () => console.log(`Server is running on port ${port}....`));
+
 app.get('/', (request, response) => response.send('Class 02 - Params'));
 
-app.listen(port, () => console.log(`Server is running on port ${port}....`));
+app.get('/user/:id/:name', (request, response) => {
+    const {id, name} = request.params;
+    response.send(`Route Params - ${name}(${id}).`);
+});
+
+app.get('/books', (request, response) => {
+    const {book, author, value} = request.query
+    response.send(`Query Params - Livro: ${book}, ${author} por R$${value}.`);
+});

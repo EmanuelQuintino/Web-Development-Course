0674f1ebac7ade5e5c1744ce343ac40f760822d7
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-02-04 20:27:12 -0300

    add test connection database

--- 8.Node.JS/class-05-databases/src/database/index.js
+++ 8.Node.JS/class-05-databases/src/database/index.js
@@ -1,4 +1,8 @@
 const knexFile = require('../../knexfile.js');
 const database =  require('knex')(knexFile.development);
 
+database.raw('SELECT 1+1 AS result')
+.then(() => console.log('Database is connected...'))
+.catch((error) => console.error(`Failed to connect Database: ${error}`));
+
 module.exports = database;

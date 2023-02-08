c36f26d883b802a8ea5fc3d783ef48224deaad49
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-02-08 13:02:09 -0300

    update class normalization

--- 7.SQL/class-04-normalization.sql
+++ 7.SQL/class-04-normalization.sql
@@ -1,52 +1,94 @@
--- 1FN, 2FN, 3FN
+-- NORMALIZATION
 
-DROP TABLE users;
-CREATE TABLE users (
+CREATE TABLE studants (
 	id SMALLINT UNSIGNED AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
     phone VARCHAR(255) NOT NULL,
     address VARCHAR(255) NOT NULL,
+    id_course SMALLINT UNSIGNED NOT NULL,
+    name_course VARCHAR(255) NOT NULL,
+    hours TINYINT UNSIGNED NOT NULL,
     
     PRIMARY KEY(id)
 );
 
-DESCRIBE users;
+DESCRIBE studants;
 
-INSERT INTO users(name, phone, address) VALUES
-('Emanuel', '(88) 8888-8888, (99) 9999-9999', 'Rua X, Bairro Y, Cidade Z'),
-('Thiago', '(88) 8888-8888, (99) 9999-9999', 'Rua X, Bairro Y, Cidade Z'),
-('Beatriz', '(88) 8888-8888, (99) 9999-9999', 'Rua X, Bairro Y, Cidade Z');
+INSERT INTO studants(
+	name, 
+    phone, 
+    address, 
+    id_course, 
+    name_course, hours) 
+VALUES
+('Emanuel', '(88) 8888-8888, (99) 9999-9999', 'Rua X, Bairro Y, Cidade Z', 1, 'Programador Web', 240),
+('Thiago', '(88) 8888-8888, (99) 9999-9999', 'Rua X, Bairro Y, Cidade Z', 1, 'Programador Web', 240),
+('Beatriz', '(88) 8888-8888, (99) 9999-9999', 'Rua X, Bairro Y, Cidade Z', 2, 'Ferramentas Digitais', 160);
 
-SELECT * FROM users;
-DROP TABLE users;
+SELECT * FROM studants;
 
-CREATE TABLE users (
+DROP TABLE courses;
+CREATE TABLE courses(
+	id SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    hours TINYINT UNSIGNED NOT NULL
+);
+
+DESCRIBE courses;
+
+INSERT INTO courses (name, hours) VALUES
+('Programador Web', 240),
+('Informática para o mundo do trabalho', 160),
+('Ferramentas Digitais', 60);
+
+SELECT * FROM courses;
+
+DROP TABLE studants;
+CREATE TABLE studants (
 	id SMALLINT UNSIGNED AUTO_INCREMENT,
 	name VARCHAR(255) NOT NULL,
     street VARCHAR(255) NOT NULL,
     neighborhood VARCHAR(255) NOT NULL,
     city VARCHAR(255) NOT NULL,
-    PRIMARY KEY(id)
+    course SMALLINT UNSIGNED,
+    
+    PRIMARY KEY(id),
+    FOREIGN KEY(course) REFERENCES courses(id)
 );
 
-INSERT INTO users(name, street, neighborhood, city) VALUES
-('Emanuel','Rua X', 'Bairro Y', 'Cidade Z'),
-('Thiago','Rua X', 'Bairro Y', 'Cidade Z'),
-('Beatriz','Rua X', 'Bairro Y', 'Cidade Z');
+DESCRIBE studants;
+
+INSERT INTO studants(
+	name, 
+    street, 
+    neighborhood, 
+    city, 
+    course) 
+VALUES
+('Emanuel','Rua X', 'Bairro Y', 'Cidade Z', 1),
+('Thiago','Rua X', 'Bairro Y', 'Cidade Z', 1),
+('Beatriz','Rua X', 'Bairro Y', 'Cidade Z', 2);
+
+SELECT * FROM studants;
+
+SELECT studants.name, courses.name 
+FROM studants INNER JOIN courses
+ON studants.course = courses.id
+ORDER BY studants.name;
 
 DROP TABLE phones;
 CREATE TABLE phones(
 	id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
     number VARCHAR(255) NOT NULL,
-    user SMALLINT UNSIGNED NOT NULL,
+    studant SMALLINT UNSIGNED NOT NULL,
   
 	PRIMARY KEY(id),
-    FOREIGN KEY(user) REFERENCES users(id) ON DELETE CASCADE
+    FOREIGN KEY(studant) REFERENCES studants(id) ON DELETE CASCADE
 );
 
 DESCRIBE phones;
 
-INSERT INTO phones (number, user) VALUES
+INSERT INTO phones (number, studant) VALUES
 ('(88) 8888-8888', 1),
 ('(99) 9999-9999', 1),
 ('(00) 0000-0000', 2),
@@ -54,9 +96,10 @@ INSERT INTO phones (number, user) VALUES
 
 SELECT * FROM phones;
 
-DELETE FROM users WHERE id = 1;
+SELECT studants.name, phones.number
+FROM phones INNER JOIN studants
+ON phones.studant = studants.id
+ORDER BY studants.name;
 
-SELECT users.name, phones.number
-FROM phones INNER JOIN users
-ON phones.user = users.id
-ORDER BY users.name;
+SELECT * FROM studants;
+DELETE FROM studants WHERE id = 1;

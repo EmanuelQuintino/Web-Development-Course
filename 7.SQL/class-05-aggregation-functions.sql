8f7f6f2e594f9435c62a6fa641cb8ef9c3175073
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-02-09 10:55:06 -0300

    add file table sales csv

--- 7.SQL/class-05-aggregation-functions.sql
+++ 7.SQL/class-05-aggregation-functions.sql
@@ -1,6 +1,6 @@
 SELECT * FROM sales;
 SELECT * FROM sales ORDER BY sale DESC;
-SELECT DISTINCT month FROM sales;
+SELECT DISTINCT name FROM sales ORDER BY name;
 SELECT * FROM sales WHERE name LIKE '_a%';
 
 SELECT * FROM sales 
@@ -8,14 +8,14 @@ WHERE month IN ('jan', 'jul','dez')
 ORDER BY month;
 
 SELECT * FROM sales 
-WHERE sale >= 200 AND sale <= 300
+WHERE sale >= 2000 AND sale <= 3000
 ORDER BY sale;
 
 SELECT * FROM sales 
-WHERE sale BETWEEN 200 AND 300
+WHERE sale BETWEEN 2000 AND 3000
 ORDER BY sale;
 
-SELECT COUNT(*) FROM sales;
+SELECT 	COUNT(*) FROM sales;
 SELECT COUNT(DISTINCT name) FROM sales;
 SELECT COUNT(DISTINCT month) FROM sales;
 
@@ -27,7 +27,7 @@ SELECT
     AVG(sale),
     STD(sale)
 FROM sales
-WHERE month = 'dez' AND sale > 300;
+WHERE month = 'dez' AND sale > 3000;
 
 SELECT month, SUM(sale) AS total 
 FROM sales GROUP BY month
@@ -36,8 +36,10 @@ ORDER BY total DESC;
 SELECT
 	name,
 	COUNT(sale),
-    SUM(sale) AS total
+    SUM(sale) AS total,
+    AVG(sale),
+	STD(sale)
 FROM sales
 GROUP BY name
-HAVING total > 40000 
+HAVING total > 350000 
 ORDER BY total DESC;

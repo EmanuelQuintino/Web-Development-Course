d1f028addd1a7a09893231b4cb60a5cadf94db2d
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-02-14 18:04:30 -0300

    update class routes

---
+++ 8-Node.JS/class-02-routes/src/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port, console.log(`Server is running on PORT ${port}...`));
+
+const routes = require('./routes');
+app.use(routes);

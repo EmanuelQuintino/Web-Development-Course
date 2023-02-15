4575f980b9244ff2842021f2d7debd3989daff7a
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-02-15 11:25:11 -0300

    update class params

--- 8-Node.JS/class-03-params/src/routes/user.routes.js
+++ 8-Node.JS/class-03-params/src/routes/user.routes.js
@@ -1,8 +1,22 @@
-const express = require('express');
-const userRouter = express();
+const {Router} = require('express');
+const userRouter = Router();
 
-userRouter.post("/", (req, res) => {
+//Route Params
+userRouter.get('/:id/:name', (req, res) => {
+    const {id, name} = req.params;
+    res.send(`User:${id} Name:${name}`);
+});
+
+//Query Params
+userRouter.get('/', (req, res) => {
+    const {id, name} = req.query;
+    res.send(`User${id} Name:${name}`);
+});
+
+//Body Params
+userRouter.post('/', (req, res) => {
     const {id, name} = req.body;
+    res.send(`User:${id} Name:${name}`);
     res.json({id, name});
 });
 

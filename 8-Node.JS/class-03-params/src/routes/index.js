52a78f1c923abc7939923f967533f589f0f294ca
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-02-15 22:50:39 -0300

    update class params

--- 8-Node.JS/class-03-params/src/routes/index.js
+++ 8-Node.JS/class-03-params/src/routes/index.js
@@ -3,7 +3,25 @@ const router = Router();
 
 router.get('/', (req, res) => res.send('Class 03 - Params'));
 
-const userRoutes = require('./user.routes');
-router.use('/user', userRoutes);
+router.get('/user', (req, res) => res.send('User'));
+
+//Route Params
+router.get('/user/:id/:name', (req, res) => {
+    const {id, name} = req.params;
+    res.send(`User:${id} Name:${name}`);
+});
+
+//Query Params
+router.get('/user', (req, res) => {
+    const {id, name} = req.query;
+    res.send(`User:${id} Name:${name}`);
+});
+
+//Body Params
+router.post('/user', (req, res) => {
+    const {id, name} = req.body;
+    res.send(`User:${id} Name:${name}`);
+    res.json({id, name});
+});
 
 module.exports = router;

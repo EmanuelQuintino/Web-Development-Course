aea23ffa902d9ff5bdbafcc8a8d71e6889207a6f
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-02-18 17:04:46 -0300

    add feedback database connection

--- 8-Node.JS/class-06-rest-api/api-prisma/src/server.js
+++ 8-Node.JS/class-06-rest-api/api-prisma/src/server.js
@@ -7,3 +7,9 @@ app.use(express.json());
 
 const routes = require('./routes');
 app.use(routes);
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+prisma.$connect()
+    .then(() => console.log('Database is connected...'))
+    .catch((error) => console.error(error));

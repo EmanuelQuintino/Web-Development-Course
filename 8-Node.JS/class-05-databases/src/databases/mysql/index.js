eb163cc0dc479ce047bee87d50c6a08ec48f142e
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-02-18 21:06:28 -0300

    add migrations

--- 8-Node.JS/class-05-databases/src/databases/mysql/index.js
+++ 8-Node.JS/class-05-databases/src/databases/mysql/index.js
@@ -9,17 +9,7 @@ const connection = mysql.createConnection({
     database: process.env.DATABASE
 });
 
-const SQL = `
-    CREATE TABLE IF NOT EXISTS users(
-        id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-        name VARCHAR(255) NOT NULL,
-        age TINYINT UNSIGNED NOT NULL,
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-        updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-        
-        PRIMARY KEY(id)
-    );`;
-
+const SQL = require('./migrations/userTable.js');
 connection.query(SQL);
 
 module.exports = connection;
---
+++ 8-Node.JS/class-05-databases/src/databases/mysql/migrations/userTable.js
@@ -0,0 +1,10 @@
+module.exports = `
+    CREATE TABLE IF NOT EXISTS users(
+        id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+        name VARCHAR(255) NOT NULL,
+        age TINYINT UNSIGNED NOT NULL,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        
+        PRIMARY KEY(id)
+    );`;

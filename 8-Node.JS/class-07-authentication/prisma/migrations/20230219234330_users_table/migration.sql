37716651a246793c78c67e62e66d28baff0708ea
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-02-19 20:48:28 -0300

    add base prisma connection

---
+++ 8-Node.JS/class-07-authentication/prisma/migrations/20230219234330_users_table/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE `Users` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `email`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

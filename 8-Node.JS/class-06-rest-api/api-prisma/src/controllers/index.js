d13d59eabbb2de08454614da2aca881c0a0dbc44
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-02-20 00:12:52 -0300

    update message errors

--- 8-Node.JS/class-06-rest-api/api-prisma/src/controllers/index.js
+++ 8-Node.JS/class-06-rest-api/api-prisma/src/controllers/index.js
@@ -13,7 +13,7 @@ module.exports = {
                 listUsers = await prisma.users.findMany();
             }
             
-            res.json({listed: listUsers ?  listUsers : 'User not found!'});
+            res.json({listed: listUsers ?  listUsers : 'User not found'});
         } catch (error) {
             next(error);
         }
@@ -24,7 +24,7 @@ module.exports = {
             const { name, email, password } = req.body;
 
             const userEmail = await prisma.users.findUnique({where: {email}});
-            if (userEmail) return res.json({alert: "Email already used!"});
+            if (userEmail) return res.json({alert: "Email already used"});
 
             const passwordHash = await bcrypt.hash(password, 10);
 
@@ -43,8 +43,8 @@ module.exports = {
             const { name, email, password, confirmPassword } = req.body;
             const user = await prisma.users.findUnique({where: {id: Number(id)}});
             
-            if (!user) return res.json({alert: "User not found!"});
-            if (!confirmPassword) return res.json({alert: "Please confirm your password!"});
+            if (!user) return res.json({alert: "User not found"});
+            if (!confirmPassword) return res.json({alert: "Please confirm your password"});
             
             const checkPassword = await bcrypt.compare(confirmPassword, user.password);
             if (checkPassword) {
@@ -58,8 +58,8 @@ module.exports = {
                 res.json({alert: "Incorrect password"});   
             }
         } catch (error) {
-            if (error.code == "P2002") return res.json({alert: "Email already used!"});
-            if (error.code == "P2025") return res.json({alert: "User not found!"});
+            if (error.code == "P2002") return res.json({alert: "Email already used"});
+            if (error.code == "P2025") return res.json({alert: "User not found"});
             next(error);
         }
     },
@@ -70,7 +70,7 @@ module.exports = {
             const deleteUsers = await prisma.users.delete({where: {id: Number(id)}});
             return res.json({deleted: deleteUsers});
         } catch (error) {
-            if (error.code == "P2025") return res.json({alert: "User not found!"});
+            if (error.code == "P2025") return res.json({alert: "User not found"});
             next(error);
         }
     }

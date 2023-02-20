3d14c45eae4d0a7293ec2a004f40c06bf86e4be0
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-02-19 22:06:22 -0300

    update prisma controllers

--- 8-Node.JS/class-06-rest-api/api-prisma/prisma/migrations/20230220004929_users_table/migration.sql
+++ 8-Node.JS/class-06-rest-api/api-prisma/prisma/migrations/20230220004929_users_table/migration.sql
@@ -1,12 +1,12 @@
 -- CreateTable
-CREATE TABLE `users` (
+CREATE TABLE `Users` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
-    `age` INTEGER NOT NULL,
-    `cpf` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
-    UNIQUE INDEX `Users_cpf_key`(`cpf`),
+    UNIQUE INDEX `email`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- 8-Node.JS/class-06-rest-api/api-prisma/src/controllers/index.js
+++ 8-Node.JS/class-06-rest-api/api-prisma/src/controllers/index.js
@@ -4,15 +4,15 @@ module.exports = {
     async read(req, res) {
         try {
             const { id } = req.query;
-            let result;
+            let listUsers;
             
             if (id) {
-                result = await prisma.users.findUnique({where: {id: Number(id)}});
+                listUsers = await prisma.users.findUnique({where: {id: Number(id)}});
             } else {
-                result = await prisma.users.findMany();
+                listUsers = await prisma.users.findMany();
             }
             
-            res.json({listed: result? result: 'User not found!'});
+            res.json({listed: listUsers ?  listUsers : 'User not found!'});
         } catch (error) {
             res.json(error);
         }
@@ -20,13 +20,13 @@ module.exports = {
 
     async create(req, res) {
         try {
-            const { name, age, cpf } = req.body;
+            const { name, email, password } = req.body;
 
-            const userCPF = await prisma.users.findUnique({where: {cpf}});
-            if (userCPF) return res.json({alert: "CPF already used!"});
+            const userEmail = await prisma.users.findUnique({where: {email}});
+            if (userEmail) return res.json({alert: "Email already used!"});
 
-            const reault = await prisma.users.create({data: {name, age, cpf}});
-            return res.json({created: reault});
+            const createUsers = await prisma.users.create({data: {name, email, password}});
+            return res.json({created: createUsers});
         } catch (error) {
             res.json(error);
         }
@@ -35,16 +35,16 @@ module.exports = {
     async update(req, res) {
         try {
             const { id } = req.params;
-            const { name, age, cpf } = req.body;
+            const { name, email, password } = req.body;
                         
-            const reault = await prisma.users.update({
+            const updateUsers = await prisma.users.update({
                 where: {id: Number(id)}, 
-                data: {name, age, cpf}
+                data: {name, email, password}
             });
 
-            return res.json({updated: reault});
+            return res.json({updated: updateUsers});
         } catch (error) {
-            if (error.code == "P2002") return res.json({alert: "CPF already used!"});
+            if (error.code == "P2002") return res.json({alert: "Email already used!"});
             if (error.code == "P2025") return res.json({alert: "User not found!"});
             res.json(error);
         }
@@ -53,8 +53,8 @@ module.exports = {
     async delete(req, res) {
         try {
             const { id } = req.params;       
-            const reault = await prisma.users.delete({where: {id: Number(id)}});
-            return res.json({deleted: reault});
+            const deleteUsers = await prisma.users.delete({where: {id: Number(id)}});
+            return res.json({deleted: deleteUsers});
         } catch (error) {
             if (error.code == "P2025") return res.json({alert: "User not found!"});
             res.json(error);

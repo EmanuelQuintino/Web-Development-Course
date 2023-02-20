b36e87101021c27e1138b1684560f2b3514049f7
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-02-20 15:56:57 -0300

    add functions controllers

--- 8-Node.JS/class-05-databases/prisma_/src/controllers/index.js
+++ 8-Node.JS/class-05-databases/prisma_/src/controllers/index.js
@@ -1,5 +1,53 @@
+const prisma = require('../databases');
+
 module.exports = {
-    read(req, res) {
-        res.json({test: "test"});
+    async read(req, res) {
+        try {
+            const { id } = req.query;
+            
+            if (id) {
+                const listUser = await prisma.users.findUnique({where: {id: Number(id)}});
+                res.json(listUser);
+            } else {
+                const listUsers = await prisma.users.findMany();
+                res.json(listUsers);
+            }
+        } catch (error) {
+            res.json({error: error.message});
+        }
+    },
+
+    async create(req, res) {
+        try {
+            const { name, age } = req.body;
+            const createUser = await prisma.users.create({data: {name, age}});
+            res.json({create: createUser});
+        } catch (error) {
+            res.json({error: error.message});
+        }
+    },
+
+    async update(req, res) {
+        try {
+            const { id } = req.params;
+            const { name, age } = req.body;
+            const updateUser = await prisma.users.update({
+                data: {name, age},
+                where: {id: Number(id)}
+            });
+            res.json({update: updateUser});
+        } catch (error) {
+            res.json({error: error.message});
+        }
+    },
+
+    async delete(req, res) {
+        try {
+            const { id } = req.params;
+            const deleteUser = await prisma.users.delete({where: {id: Number(id)}});
+            res.json({delete: deleteUser});
+        } catch (error) {
+            res.json({error: error.message});
+        }
     }
 }
--- 8-Node.JS/class-05-databases/prisma_/src/server.js
+++ 8-Node.JS/class-05-databases/prisma_/src/server.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.listen(3000, console.log('Server is running on PORT 3000'));
 
+app.use(express.json());
+
 const routes = require('./routes');
 app.use(routes);
 

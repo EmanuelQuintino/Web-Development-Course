91894f550381be771a3e4db3a206db80f798cd6b
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-02-19 23:43:44 -0300

    add update validations

--- 8-Node.JS/class-06-rest-api/api-prisma/src/controllers/index.js
+++ 8-Node.JS/class-06-rest-api/api-prisma/src/controllers/index.js
@@ -40,16 +40,23 @@ module.exports = {
     async update(req, res) {
         try {
             const { id } = req.params;
-            const { name, email, password } = req.body;
-
-            const passwordHash = await bcrypt.hash(password, 10);
-                        
-            const updateUsers = await prisma.users.update({
-                where: {id: Number(id)}, 
-                data: {name, email, password: passwordHash}
-            });
-
-            return res.json({updated: updateUsers});
+            const { name, email, password, confirmPassword } = req.body;
+            const user = await prisma.users.findUnique({where: {id: Number(id)}});
+            
+            if (!user) return res.json({alert: "User not found!"});
+            if (!confirmPassword) return res.json({alert: "Please confirm your password!"});
+            
+            const checkPassword = await bcrypt.compare(confirmPassword, user.password);
+            if (checkPassword) {
+                const passwordHash = await bcrypt.hash(password, 10);          
+                const updateUsers = await prisma.users.update({
+                    where: {id: Number(id)}, 
+                    data: {name, email, password: passwordHash}
+                });
+                return res.json({updated: updateUsers});
+            } else {
+                res.json({alert: "Incorrect password"});   
+            }
         } catch (error) {
             if (error.code == "P2002") return res.json({alert: "Email already used!"});
             if (error.code == "P2025") return res.json({alert: "User not found!"});

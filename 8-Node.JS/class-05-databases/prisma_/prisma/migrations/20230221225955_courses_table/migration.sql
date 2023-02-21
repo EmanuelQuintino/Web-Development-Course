7298f05b067927eb43bb551d1564b67b39b3158a
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-02-21 20:00:53 -0300

    add migrations

--- 8-Node.JS/class-05-databases/prisma_/prisma/migrations/20230221225955_courses_table/migration.sql
+++ 8-Node.JS/class-05-databases/prisma_/prisma/migrations/20230221225955_courses_table/migration.sql
@@ -1,12 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE `users`;
-
 -- CreateTable
 CREATE TABLE `Courses` (
     `id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,

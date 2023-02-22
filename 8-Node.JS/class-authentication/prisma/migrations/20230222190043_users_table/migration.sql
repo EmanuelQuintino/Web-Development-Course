db8448da6f4cc3fa82e53a7ec06bae56a4a01f2a
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-02-22 16:02:19 -0300

    add authentication migrations

---
+++ 8-Node.JS/class-authentication/prisma/migrations/20230222190043_users_table/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `users` (
+    `id` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `created_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `updated_at` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+
+    UNIQUE INDEX `email`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

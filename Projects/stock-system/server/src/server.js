02db25bb46801da8a840387b27b595d93b527240
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-02-26 09:24:36 -0300

    add base server

---
+++ Projects/stock-system/server/src/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.listen(3000, console.log('Server is running on port 3000'));
+
+app.use(express.json());
+
+const prisma = require('./database');
+prisma.$connect()
+    .then(() => console.log('Database is connected...'))
+    .catch((error) => console.error(error));

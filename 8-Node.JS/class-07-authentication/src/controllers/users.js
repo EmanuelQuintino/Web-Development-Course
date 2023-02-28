d9f4c9022e37bbe1ff1e7a6195713c32add893c4
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-02-28 11:02:33 -0300

    add password hash

--- 8-Node.JS/class-07-authentication/src/controllers/users.js
+++ 8-Node.JS/class-07-authentication/src/controllers/users.js
@@ -1,4 +1,5 @@
 const prisma = require('../databases');
+const bcrypt = require('bcrypt');
 
 module.exports = {
     async read (req, res) {
@@ -14,7 +15,7 @@ module.exports = {
                 return res.json(listUsers); 
             }
         } catch (error) {
-            return res.json(error);
+            return res.json(error.message);
         }
     },
 
@@ -27,10 +28,12 @@ module.exports = {
             const userEmail = await prisma.users.findUnique({where: {email}});
             if (userEmail) return res.json('Email already used');
 
-            const createUser =await prisma.users.create({data: {email, password}});
+            const passwordHash = await bcrypt.hash(password, 10);
+
+            const createUser =await prisma.users.create({data: {email, password: passwordHash}});
             return res.status(201).json(`User ${createUser.id} created`);
         } catch (error) {
-            return res.json(error);
+            return res.json(error.message);
         }
     },
 
@@ -41,19 +44,22 @@ module.exports = {
             if (!password) return res.status(400).json('Password is required');
             
             const { id } = req.params;
-            const user = prisma.users.update({where: {id: Number(id)}});
+            const user = await prisma.users.findUnique({where: {id: Number(id)}});
             if(!user) return res.json('User not found').status(400);
 
             const userEmail = await prisma.users.findUnique({where: {email}});
-            if (userEmail) return res.json('Email already used');
+            console.log(userEmail.email, user.email);
+            if (userEmail && (userEmail.email != user.email)) return res.json('Email already used');
+
+            const passwordHash = await bcrypt.hash(password, 10);
 
             const updateUser = await prisma.users.update({
-                data: {email, password},
+                data: {email, password: passwordHash},
                 where: {id: Number(id)}
             });
             return res.json(`User ${updateUser.id} updated`);
         } catch (error) {
-            return res.json(error);
+            return res.json(error.message);
         }
     },
 
@@ -66,7 +72,7 @@ module.exports = {
             const deleteUser = await prisma.users.delete({where: {id: Number(id)}});
             return res.json(`User ${deleteUser.id} deleted`);
         } catch (error) {
-            return res.json(error);
+            return res.json(error.message);
         }
     }
 }

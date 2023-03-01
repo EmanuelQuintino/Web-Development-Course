679b90eab68f1460ffe285b28019f02c91b74963
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-03-01 18:29:27 -0300

    update status code

--- 8-Node.JS/class-07-authentication/src/controllers/auth.js
+++ 8-Node.JS/class-07-authentication/src/controllers/auth.js
@@ -11,13 +11,13 @@ module.exports = {
             if (!password) return res.status(400).json('Password is required');
 
             const user = await prisma.users.findUnique({where: {email}});
-            if (!user) return res.status(400).json('Email or Password incorrect');
+            if (!user) return res.status(401).json('Email or Password incorrect');
 
             const passwordCheck = await bcrypt.compare(password, user.password);
-            if(!passwordCheck) return res.status(400).json('Email or Password incorrect');
+            if(!passwordCheck) return res.status(401).json('Email or Password incorrect');
 
             const token = jwt.sign({id: String(user.id)}, secret, {expiresIn});
-            return res.json({id: user.id, status: "Authenticate", token});
+            return res.json({id: user.id, status: "Authenticated", token});
         } catch (error) {
             return console.error(error.message);
         }

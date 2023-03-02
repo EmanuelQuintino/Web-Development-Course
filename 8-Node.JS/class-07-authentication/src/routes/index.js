0a54dcc126d47f40be8cfbadbfa5e10d5f0c8713
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-03-02 11:25:17 -0300

    add consult user email

--- 8-Node.JS/class-07-authentication/src/routes/index.js
+++ 8-Node.JS/class-07-authentication/src/routes/index.js
@@ -1,18 +1,21 @@
 const { Router } = require('express');
 const router = Router();
-const authMiddleware = require('../middlewares/auth');
-
 const userControllers = require('../controllers/users');
+const loginControllers = require('../controllers/auth');
+const authMiddleware = require('../middlewares/auth');
+const prisma = require('../databases');
+
 router.get('/users', userControllers.read);
 router.post('/users', userControllers.create);
 router.put('/users/:id', userControllers.update);
 router.delete('/users/:id', userControllers.delete);
 
-const loginControllers = require('../controllers/auth');
 router.post('/login', loginControllers.auth);
 
-router.get('/system', authMiddleware, (req, res) => {
-    return res.json({system: true, status: "authorized", user: req.userID});
+router.get('/system', authMiddleware, async (req, res) => {
+    const id = req.userID;
+    const user = await prisma.users.findUnique({where: {id: Number(id)}});
+    return res.json({system: true, id, email: user.email, status: "authorized"});
 });
 
 module.exports = router;

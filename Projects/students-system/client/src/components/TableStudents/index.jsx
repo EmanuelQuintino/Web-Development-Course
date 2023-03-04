f4ffaff6009a0c338011fcd4dbe053edd0a93752
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-03-04 10:27:22 -0300

    add students search bar

--- Projects/students-system/client/src/components/TableStudents/index.jsx
+++ Projects/students-system/client/src/components/TableStudents/index.jsx
@@ -3,20 +3,19 @@ import { useState, useEffect } from "react"
 import axios from "axios";
 
 export function TableStudents() {
-    const [listStudents, setListStudents] = useState();
-    const [searchStudent, setSearchStudent] = useState();
-
-    async function fetchStudents() {
-        const API = "http://localhost:3000/students"
-        axios.get(API)
-            .then((res) => setListStudents(res.data))
-            .catch((error) => console.error(error));
-    }
+    const [listStudents, setListStudents] = useState([]);
+    const [searchStudent, setSearchStudent] = useState('');
     
     useEffect(() => {
-        fetchStudents()
-    }, []);    
-
+        const API = "http://localhost:3000/students"
+        axios.get(API)
+        .then((res) => setListStudents(res.data))
+        .catch((error) => console.error(error));
+    }, []);
+    
+    const filterStudents = listStudents.filter((student) => {
+        return student.name.toLowerCase().includes(searchStudent.toLowerCase());
+    })
     return (
         <Container>
             <article>
@@ -26,17 +25,13 @@ export function TableStudents() {
                         type="text" 
                         placeholder="Nome do aluno" 
                         onChange={(event) => setSearchStudent(event.target.value)}
+                        value={searchStudent}
                     />
-
-                    <p>{searchStudent}</p>
-
                 </section>
 
                 <section>
-                    {listStudents && 
-                        listStudents.map((student, index) => {
-                          return <p key={index}>{student.name}</p>
-                        })
+                    { listStudents &&
+                        filterStudents.map((student, index) => <p key={index}>{student.name}</p>) 
                     }
                 </section>
             </article>

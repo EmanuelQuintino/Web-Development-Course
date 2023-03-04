66cdc67ee3be2699af5dfd83f956b52bd93724a6
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-03-04 16:49:39 -0300

    add footer style

--- Projects/students-system/client/src/App/style.js
+++ Projects/students-system/client/src/App/style.js
@@ -10,11 +10,19 @@ export const MainArticle = styled.article`
     max-width: 32.0rem;
     margin: 0 auto;
     padding: 2.4rem .0rem;
-
     text-align: justify;
 `;
 
 export const Footer = styled.div`
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
     background: ${({theme}) => theme.colors.background};
     border-top: .4rem solid ${({theme}) => theme.colors.neutral8};
+    color: white;
+    font-size: 1.2rem;
+    font-weight: 500;
+    padding: 1.6rem;
+    text-align: center;
 `;

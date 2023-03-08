a0791bae2e52aa00192e2a495b7eccf73f27c7ba
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-03-07 23:17:43 -0300

    add home content img and lorem

--- Projects/students-system/client/src/pages/Home/index.jsx
+++ Projects/students-system/client/src/pages/Home/index.jsx
@@ -1,7 +1,16 @@
+import { Container } from "./style"
+
 export function Home() {
   return (
-    <div className="container">
+    <Container>
       <h1>Home</h1>
-    </div>
+      <img src="https://images.pexels.com/photos/1462630/pexels-photo-1462630.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum earum enim adipisci, sequi, ipsum ab sint voluptate ratione facere deserunt rerum voluptas placeat numquam eius! Veritatis magni perspiciatis odit eius.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus accusamus odio similique quaerat minima alias aperiam corrupti est, eius architecto. Totam, reiciendis sapiente itaque mollitia temporibus ducimus doloremque quos ea.
+      </p>
+    </Container>
   )
 }
---
+++ Projects/students-system/client/src/pages/Home/style.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    img {
+        width: 32.0rem;
+    }
+
+    p {
+        margin-top: 1.6rem;
+    }
+`; 

1787ccb2cb581d3bee78d775476cde0cddb91b4b
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-03-08 23:04:50 -0300

    add about technologies

--- Projects/students-system/client/src/pages/About/index.jsx
+++ Projects/students-system/client/src/pages/About/index.jsx
@@ -4,6 +4,32 @@ export function About() {
   return (
     <Container>
       <h1>Sobre</h1>
+      <p>
+        Projeto criado para conclusão do Curso de Desenvolvimento Web Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae dolor voluptates quisquam temporibus incidunt ullam itaque! Quibusdam laudantium at rerum! Repellendus laboriosam alias accusamus quia voluptate blanditiis explicabo labore doloribus.
+      </p>
+
+      <section>
+      <h2>Tecnologias</h2>
+        <p>
+          <h4>React</h4>
+          <ul>
+            <li>Style Components</li>
+            <li>React Router Dom</li>
+            <li>React Icons</li>
+            <li>UseForm e Yup</li>
+            <li>Axios</li>
+            <li>Bootstrap</li>
+          </ul>
+        </p>
+
+        <p>
+          <h4>Node</h4>
+          <ul>
+            <li>Express</li>
+            <li>Prisma</li>
+          </ul>
+        </p>
+      </section>
     </Container>
   )
 }

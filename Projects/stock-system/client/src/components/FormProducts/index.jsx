857e7cfc9e8dd91d93d0c44bb51ca79f7af95308
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-03-12 20:09:07 -0300

    update form layout

--- Projects/stock-system/client/src/components/FormProducts/index.jsx
+++ Projects/stock-system/client/src/components/FormProducts/index.jsx
@@ -1,37 +1,79 @@
+import { Container } from './style';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { useForm } from "react-hook-form";
 
 export function FormProducts() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const API = "http://localhost:3000/products";
+  // console.log(API);
+  console.log(errors);
+  
+  function onSubmit(data) {
+    console.log(data);
+  }
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="name">
-        <Form.Label>Nome</Form.Label>
-        <Form.Control type="text" placeholder="Nome do produto" />
-      </Form.Group>
+    <Container>
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Nome</Form.Label>
+          <Form.Control 
+            type="text" 
+            placeholder="Nome do produto" 
+            {...register("name", { required: true })}
+          />
+          {errors.name && <span className='error'>Campo obrigatório</span>}
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="desciption">
-        <Form.Label>Descrição</Form.Label>
-        <Form.Control type="text" placeholder="Descreva detalhes do produto" />
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="desciption">
+          <Form.Label>Descrição</Form.Label>
+          <Form.Control 
+            type="text" 
+            placeholder="Descreva detalhes do produto"
+            {...register("description", { required: true })} 
+          />
+          {errors.description && <span className='error'>Campo obrigatório</span>}
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="desciption">
-        <Form.Label>Preço</Form.Label>
-        <Form.Control type="text" placeholder="Preço R$" />
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="price">
+          <Form.Label>Preço</Form.Label>
+          <Form.Control 
+            type="text" 
+            placeholder="R$ 3.50"
+            {...register("price", { required: true })}
+          />
+          {errors.price && <span className='error'>Campo obrigatório</span>}
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="desciption">
-        <Form.Label>Vencimento</Form.Label>
-        <Form.Control type="date"/>
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="expiresIn">
+          <Form.Label>Vencimento</Form.Label>
+          <Form.Control 
+            type="date" 
+            {...register("expiresIn", { required: true })}
+          />
+          {errors.expiresIn && <span className='error'>Campo obrigatório</span>}
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="desciption">
-        <Form.Label>Imagem</Form.Label>
-        <Form.Control type="text" placeholder="Link da imagem" />
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="linkImage">
+          <Form.Label>Imagem</Form.Label>
+          <Form.Control 
+            type="text" 
+            placeholder="Link da imagem" 
+            {...register("linkImage", { required: true })}
+          />
+          {errors.linkImage && <span className='error'>Campo obrigatório</span>}
+        </Form.Group>
 
-      <Button variant="primary" type="submit">
-        Cadastrar
-      </Button>
-    </Form>
+        <Button variant="primary" type="submit" style={{
+          marginTop: "1.6rem",
+          width: "100%",
+          padding: ".4rem",
+          fontSize: "1.4rem" 
+        }}>
+          Cadastrar
+        </Button>
+      </Form>
+    </Container>
   );
 }
---
+++ Projects/stock-system/client/src/components/FormProducts/style.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.section `
+    input {
+        font-size: 1.4rem;
+    }
+    
+    label {
+        margin-top: 1.0rem;
+    }
+
+    .error {
+        color: red;
+        display: block;
+        position: absolute;
+        font-size: 1.0rem;
+        margin-top: .2rem;
+    }
+`; 

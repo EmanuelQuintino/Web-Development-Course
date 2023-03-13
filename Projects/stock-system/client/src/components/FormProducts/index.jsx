09a23c9a577bc0d799df4d077967b3d4390c2c67
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-03-13 18:43:01 -0300

    add form validation

--- Projects/stock-system/client/src/components/FormProducts/index.jsx
+++ Projects/stock-system/client/src/components/FormProducts/index.jsx
@@ -19,49 +19,71 @@ export function FormProducts() {
           <Form.Label>Nome</Form.Label>
           <Form.Control 
             type="text" 
-            placeholder="*Nome do produto" 
-            {...register("name", { required: true })}
+            placeholder="Nome do produto" 
+            {...register("name", { 
+              required: "Campo obrigatório",
+              pattern: {value: /[A-Za-z]/, message: "Somente texto é permitido"}, 
+              maxLength: {value: 60, message: "Número máximo de caracteres é 60"}
+            })}
           />
-          {errors.name && <span className='error'>Campo obrigatório</span>}
+          {errors.name && <span className='error'>{errors.name.message}</span>}
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="desciption">
           <Form.Label>Descrição</Form.Label>
           <Form.Control 
             type="text" 
-            placeholder="*Descreva detalhes do produto"
-            {...register("description", { required: true })} 
+            placeholder="Descreva detalhes do produto"
+            {...register("description", { 
+              required: "Campo obrigatório", 
+              maxLength: {value: 60, message: "Número máximo de caracteres é 60"}
+            })} 
           />
-          {errors.description && <span className='error'>Campo obrigatório</span>}
+          {errors.description && <span className='error'>{errors.description.message}</span>}
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="price">
           <Form.Label>Preço</Form.Label>
           <Form.Control 
-            type="text" 
-            placeholder="*R$ 3.50"
-            {...register("price", { required: true })}
+            type="number"
+            step={0.1}
+            min = {0}
+            placeholder="R$ 3.50"
+            inputMode='numeric'
+            autoComplete='cc-number'
+            {...register("price", { 
+              required: "Campo obrigatório",
+              pattern: {value: /[0-9]/, message: "Somente números é permitido"}, 
+              maxLength: {value: 60, message: "Número máximo de caracteres é 60"}
+            })}
           />
-          {errors.price && <span className='error'>Campo obrigatório</span>}
+          {errors.price && <span className='error'>{errors.price.message}</span>}
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="expiresIn">
           <Form.Label>Data de vencimento</Form.Label>
           <Form.Control 
             type="date" 
-            {...register("expiresIn", { required: true })}
+            {...register("expiresIn", { 
+              required: "Campo obrigatório",
+              pattern: {value: /\d{4}[/-]\d{2}[/-]\d{2}/, message: "Insira uma data válida"}, 
+              maxLength: {value: 10, message: "Número máximo de caracteres é 10"}
+            })}
           />
-          {errors.expiresIn && <span className='error'>Campo obrigatório</span>}
+          {errors.expiresIn && <span className='error'>{errors.expiresIn.message}</span>}
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="linkImage">
           <Form.Label>Imagem</Form.Label>
           <Form.Control 
             type="text" 
-            placeholder="*Link da imagem" 
-            {...register("linkImage", { required: true })}
+            placeholder="Link da imagem" 
+            {...register("linkImage", { 
+              required: "Campo obrigatório", 
+              maxLength: {value: 255, message: "Número máximo de caracteres é 255"}
+            })}
           />
-          {errors.linkImage && <span className='error'>Campo obrigatório</span>}
+          {errors.linkImage && <span className='error'>{errors.linkImage.message}</span>}
         </Form.Group>
 
         <Button variant="primary" type="submit" style={{

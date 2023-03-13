20cdff44f53d52006b8999c89db96f2ddc2731d1
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-03-13 10:39:26 -0300

    update about text

--- Projects/students-system/client/src/pages/About/index.jsx
+++ Projects/students-system/client/src/pages/About/index.jsx
@@ -5,7 +5,7 @@ export function About() {
     <Container>
       <h1>Sobre</h1>
       <p>
-        Projeto Full Stack criado para conclusão do Curso de Desenvolvimento Web. Aborda estilos em JavaScript e pré processadores, rotas e menu de navegação e página de error 404, também conta com formulários e validação de campos para cadastro. Na página de Alunos, foi colocado um input de busca avançada por ID, Nome, Email, Cidade e CPF que mostra - em uma tabela - os dados dos alunos com opção de editar ou deletar se preferir. Algumas das tecnologias usados foram:
+        Projeto Full Stack criado para conclusão do Curso de Desenvolvimento Web. Aborda estilos em JS e pré processadores, rotas, menu de navegação e página de error, também conta com formulários e validação de campos. Na página de Alunos, foi colocado um input de busca avançada por ID, Nome e Cidade que mostra - em uma tabela - os dados dos alunos com opção de editar ou deletar se preferir. Algumas das tecnologias usadas foram:
       </p>
 
       <section>

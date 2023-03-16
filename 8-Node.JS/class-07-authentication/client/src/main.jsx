b880ad6c81856439c5586503361505907419b6a2
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-03-16 19:26:24 -0300

    add style reset

--- 8-Node.JS/class-07-authentication/client/src/main.jsx
+++ 8-Node.JS/class-07-authentication/client/src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import { Reset } from './style/reset'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Reset/>
   </React.StrictMode>,
 )

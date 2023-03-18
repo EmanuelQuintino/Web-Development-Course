343b393b52bd4a5b2290d4848b659d1e2de5bdeb
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-03-17 23:15:26 -0300

    fixed p > h3

--- Projects/students-system/client/src/pages/About/index.jsx
+++ Projects/students-system/client/src/pages/About/index.jsx
@@ -3,13 +3,14 @@ import { Container } from "./style"
 export function About() {
   return (
     <Container>
-      <h1>Sobre</h1>
-      <p>
-        Projeto Full Stack criado para conclusão do Curso de Desenvolvimento Web. Aborda estilos em JS e pré-processadores, rotas, menu de navegação e página de error, também conta com formulários e validação de campos. Na página de Alunos, foi colocado um input de busca avançada por ID, Nome e Cidade que mostra - em uma tabela - os dados dos alunos com opção de editar ou deletar se preferir. Algumas das tecnologias usadas foram:
-      </p>
-
       <section>
-        <p>
+            <h1>Sobre</h1>
+            <p>
+      Projeto Full Stack criado para conclusão do Curso de Desenvolvimento Web. Aborda estilos em JS e pré-processadores, rotas, menu de navegação e página de error, também conta com formulários e validação de campos. Na página de Alunos, foi colocado um input de busca avançada por ID, Nome e Cidade que mostra - em uma tabela - os dados dos alunos com opção de editar ou deletar se preferir. Algumas das tecnologias usadas foram:
+          </p>
+        </section>
+
+        <section>
           <h3>Front-End</h3>
           <ul>
             <li>React</li>
@@ -20,18 +21,17 @@ export function About() {
             <li>Axios</li>
             <li>Bootstrap</li>
           </ul>
-        </p>
+        </section>
 
-        <p>
-          <h4>Back-End</h4>
+        <section>
+          <h3>Back-End</h3>
           <ul>
             <li>MySQL</li>
             <li>Node</li>
             <li>Express</li>
             <li>Prisma</li>
           </ul>
-        </p>
-      </section>
+        </section>
     </Container>
   )
 }

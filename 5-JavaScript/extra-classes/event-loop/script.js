ed6aeb4639d7f0c5e4bd67a818d1b6380bd05c7b
EmanuelQuintino <emanuelquintino@hotmail.com>
2023-04-10 14:56:45 -0300

    add example promise all

--- 5-JavaScript/extra-classes/event-loop/script.js
+++ 5-JavaScript/extra-classes/event-loop/script.js
@@ -1,5 +1,5 @@
 // Call Stack
-console.log(1);
+// console.log(1);
 
 // Call Stack
 // for (let index = 0; index < 200000; index++) {
@@ -7,26 +7,65 @@ console.log(1);
 // }  
 
 // Microtask
-queueMicrotask(() => {
-  console.log(2);
-});
+// queueMicrotask(() => {
+//   console.log(2);
+// });
 
 // Tasks
-setTimeout(() => {
-  console.log(3);
-}, 2000);
+// setTimeout(() => {
+//   console.log(3);
+// }, 2000);
 
 // Microtask
-Promise.resolve(true).then(() => {
-  console.log(4);
-});
+// Promise.resolve(true).then(() => {
+//   console.log(4);
+// });
 
-function sum(a, b) {
-  queueMicrotask(() => {
-    console.log(6);
-  })
-  return a + b;
-}
+// function sum(a, b) {
+//   queueMicrotask(() => {
+//     console.log(6);
+//   })
+//   return a + b;
+// }
 
 // Call Stack
-console.log(sum(2, 3));
+// console.log(sum(2, 3));
+
+async function fetchUser() {
+  const API = user => `https://api.github.com/users/${user}`;
+  // const users = ["EmanuelQuintino", "Maria", "Carla"];
+
+  console.time();
+  // const user1 = await fetch(API('EmanuelQuintino'));
+  // const user2 = await fetch(API('Maria'));
+  // const user3 = await fetch(API('Carla'));
+  const [
+    user1, 
+    user2, 
+    user3
+  ] = await Promise.all([
+    fetch(API('EmanuelQuintino')),
+    fetch(API('Maria')),
+    fetch(API('Carla'))
+  ]);
+
+  // for (let i = 0; i < users.length; i++) {
+    // fetch(API(users[i])).then((response) => console.log(response.data));
+  // }
+
+  // users.forEach((user) => {
+    // fetch(API(user)).then((response) => console.log(response.data));
+  // });
+
+  // users.map((user) => {
+  //   fetch(API(user)).then((response) => console.log(response.data));
+  // });
+
+  console.log(user1);
+  console.log(user2);
+  console.log(user3);
+  // console.log(users);/
+  console.timeEnd();
+}
+
+fetchUser();

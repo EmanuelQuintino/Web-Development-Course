879ca477e67cd4cd500027e6f28c2d2e388c2720
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-09-03 11:07:33 -0300

    add class closures

---
+++ 5-JavaScript/extra-classes/closures/closure.js
@@ -0,0 +1,9 @@
+// Closures
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Closures
+
+let num = 10;
+
+export function incrementNum() {
+  let num = 50;
+  return num + 1;
+}
---
+++ 5-JavaScript/extra-classes/closures/script.js
@@ -0,0 +1,4 @@
+import { incrementNum } from "./closure.js";
+
+let num = 20;
+console.log(incrementNum());

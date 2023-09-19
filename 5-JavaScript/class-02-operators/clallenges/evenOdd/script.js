81ab1378399c8c6a462df158b2b9e7afa9601eff
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-09-18 22:40:18 -0300

    update challenges format

--- 5-JavaScript/class-02-operators/clallenges/evenOdd/script.js
+++ 5-JavaScript/class-02-operators/clallenges/evenOdd/script.js
@@ -2,7 +2,7 @@
 // Receba o número desejado do usuário via promp
 // Escreve na tela o número e se ele é par ou é ímpar
 
-const numUser = Number(prompt("Digite um número para saber se é par ou é ímpar: "))
-const result = numUser % 2 == 0 ? "Par" : "Ímpar"
+const numUser = Number(prompt("Digite um número para saber se é par ou é ímpar: "));
+const result = numUser % 2 == 0 ? "Par" : "Ímpar";
 
-window.alert(`O número ${numUser} é ${result}`)
+window.alert(`O número ${numUser} é ${result}`);

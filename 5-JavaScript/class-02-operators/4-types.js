27743f8d7244d8ff6ca7c60c12ec96af8299ec72
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-09-20 10:29:16 -0300

    update type operators examples

--- 5-JavaScript/class-02-operators/4-types.js
+++ 5-JavaScript/class-02-operators/4-types.js
@@ -15,19 +15,19 @@ console.log(n1);
 console.log(++n1);
 console.log(typeof n2);
 
-const fruits = ["banana", "maçã", "Uva"];
+const fruits = ["banana", "maçã", "uva"];
 delete fruits[1];
 console.log(fruits);
 
 // Ternary
-test ? true : false;
+// test ? true : false;
 
-console.log(n1 > n2 ? "Yes" : "No");
+console.log(n1 > n2 ? "true" : "false");
 
-let avarege = 7;
-console.log(avarege >= 7 ? "Aprovado" : "Reprovado");
+let average = 7;
+console.log(average >= 7 ? "aprovado" : "reprovado");
 
-// * Cuidado com valores Truthy e False onde o booleano é considerado obrigatório (condicionais e loops).
+// Be careful with Truthy and Falsy values where Boolean is considered mandatory (conditionals and loops).
 
 // Truthy               Falsy
 // 1, 1.5, -1           0
@@ -36,9 +36,9 @@ console.log(avarege >= 7 ? "Aprovado" : "Reprovado");
 // []                   undefined
 // Infinity, -Infinity  NaN
 
-console.log(1 ? "yes" : "no");
-console.log("" ? "yes" : "no");
-console.log("0" ? "yes" : "no");
-console.log({} ? "yes" : "no");
-console.log(null ? "yes" : "no");
-console.log(Infinity ? "yes" : "no");
+console.log(-1 ? "true" : "false");
+console.log("" ? "true" : "false");
+console.log("0" ? "true" : "false");
+console.log({} ? "true" : "false");
+console.log(null ? "true" : "false");
+console.log(-Infinity ? "true" : "false");

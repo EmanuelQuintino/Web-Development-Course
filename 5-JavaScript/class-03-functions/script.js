822599582e871c1a3493d0326acc27cb61c4f2cb
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-09-22 10:08:45 -0300

    update format code

--- 5-JavaScript/class-03-functions/script.js
+++ 5-JavaScript/class-03-functions/script.js
@@ -1,66 +1,53 @@
+// Referência: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions
+
 // Function Statements
 
+// Named
+function myTest(text /*parameters*/) {
+  console.log(text);
+}
 
-// >>>> Named <<<<
+myTest("Testando a função"); //arguments
 
-// function myTest(text) { //parameters
-//     console.log(text)
-// }
+function add(num1, num2) {
+  // return num1 + num2
+  let soma = num1 + num2; //key operator
+  return soma;
+}
 
-// myTest("Testando a função") //arguments
+// Anonymous
+const add = function (num1, num2) {
+  return num1 + num2;
+};
 
+// Arrow
+const add = (num1, num2) => num1 + num2;
 
-// function somar(num1, num2) {
-//     // return num1 + num2
+console.log(add(2, 5));
+console.log(add);
 
-//     let soma = num1 + num2 //key operator
-//     return soma
-// }
-
-
-// >>>> Anonymous <<<<
-
-// const somar = function (num1, num2) {
-//     return num1 + num2
-// }
-
-
-// >>>>> Arrow <<<<<
-
-const somar = (num1, num2) => num1 + num2
-
-// console.log(somar(2, 5))
-// console.log(soma)
-
-
-// >>>>> Callback <<<<<<
-
+// Callback
 function show(item) {
-    console.log(item);
+  console.log(item);
 }
 
-function sum(num1, num2, callback) {
-    let sum = num1 + num2
-    callback(sum)
+function addShow(num1, num2, callback) {
+  let sum = num1 + num2;
+  callback(sum);
 }
 
-sum(2, 3, show)
+addShow(2, 3, show);
 
+// Constructor
+function Car(brandName, motorPower) {
+  this.brand = brandName;
+  this.motor = motorPower;
+}
 
-// >>>> Constructor <<<<<
+const siena = new Car("Fiat", 1.4);
+const onix = new Car("Chevrolet", 1.4);
+const polo = new Car("Volkswagen", 1.0);
 
-// function Car(brandName, motorPower) {
-//     this.brand = brandName
-//     this.motor = motorPower
-// }
-
-// const siena = new Car("Fiat", 1.4)
-// const onix = new Car("Chevrolet", 1.4)
-// const polo = new Car("Volkswagen", 1.0)
-
-// console.log(siena.brand)
-// console.log(onix.motor)
-// console.log(polo)
-
-
-// Referência: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions
+console.log(siena.brand);
+console.log(onix.motor);
+console.log(polo);

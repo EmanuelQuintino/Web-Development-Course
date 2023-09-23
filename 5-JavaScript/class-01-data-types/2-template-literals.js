af38f2c54eb8d43e0ab4b212ae41d85f8e96b885
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-09-23 18:12:25 -0300

    add example type coersion

--- 5-JavaScript/class-01-data-types/2-template-literals.js
+++ 5-JavaScript/class-01-data-types/2-template-literals.js
@@ -7,8 +7,7 @@ num1 = Number(num1); // Type Conversion (Typecasting)
 // Number() (função construtora para converter um dado em número)
 
 console.log(num1 + num2); // Type Coersion
-
-let addition = num1 + num2;
+console.log(Number("10") + 20);
 
 // Concatenation (grouping operator)
 console.log("A soma é: " + num1 + num2);
@@ -18,4 +17,5 @@ console.log("A soma é: " + (num1 + num2));
 console.log("A soma de " + num1 + " e de " + num2 + " é igual a " + (num1 + num2));
 
 // Template Literals (Strings)
+let addition = num1 + num2;
 console.log(`A soma de ${num1} e de ${num2} é igual a ${addition}`);

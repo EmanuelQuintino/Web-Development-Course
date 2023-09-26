9060dea5e3c9a97db653cfd6442a75f00a155f05
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-09-26 00:29:24 -0300

    add challenge textWordCount

---
+++ 5-JavaScript/class-04-data-manipulation/challenges/1-text-word-count.js
@@ -0,0 +1,10 @@
+// Crie uma função que retorne quantas palavras tem em uma frase
+
+function textWordCount(text) {
+  return text.trim().split(" ").length;
+}
+
+const text = "Foque no processo, não pule etapas!";
+const numberWordText = textWordCount(text);
+
+console.log(numberWordText);

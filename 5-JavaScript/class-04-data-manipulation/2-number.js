defa78629fc9d5b93802ef21d6a717884036587f
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-09-26 21:36:41 -0300

    update examples number manipulation

--- 5-JavaScript/class-04-data-manipulation/2-number.js
+++ 5-JavaScript/class-04-data-manipulation/2-number.js
@@ -1,10 +1,11 @@
 //  Number Manipulation
 
 let num1 = 7.93467;
-let num2 = -5;
+let num2 = -5249582749582489795578;
 
 console.log(num1);
 console.log(parseInt(num1));
+console.log(parseInt(num2)); // limited
 console.log(parseFloat(num2));
 console.log(String(num1).length);
 console.log(String(num1).replace(".", "").length);
@@ -25,7 +26,11 @@ console.log(num1.toLocaleString("pt-br", { style: "currency", currency: "jpy" })
 
 console.log(Math.sqrt(81));
 console.log(Math.PI);
-console.log(Math.abs(num2));
+console.log(Math.E);
+console.log(Math.abs(num1));
+
+console.log(Math.max(2, 3));
+console.log(Math.min(2, 3));
 
 const numbers = [12, 34, 4, 98, 32, 56, 178, 12, 43, 65];
 console.log(Math.max(...numbers)); //spread
@@ -35,4 +40,11 @@ console.log(Math.ceil(num1));
 console.log(Math.round(num1));
 console.log(Math.floor(num1));
 
-console.log(Math.round(Math.random() * 10));
+console.log(Math.round(2.4));
+console.log(Math.round(2.5));
+console.log(Math.floor(2.9999));
+console.log(Math.ceil(2.0001));
+
+console.log(Math.round(Math.random() * 10)); // 0-10
+console.log(Math.floor(Math.random() * 10)); // 0-9
+console.log(Math.floor(Math.random() * 10) + 1); // 1-10

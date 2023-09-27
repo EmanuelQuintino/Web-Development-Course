bafae69ff804b2757d8c49c69bb07d9a49564ad4
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-09-26 22:17:55 -0300

    add challenge random number

---
+++ 5-JavaScript/class-04-data-manipulation/challenges/3-random-number.js
@@ -0,0 +1,8 @@
+// Crie uma função capaz de retornar um número aleatório
+// dentro de um intervalo determinado por parâmetros min(mínimo) e max(máximo).
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(randomNumber(1, 10));

3e489bb161ae888d23c2aac927967b5a0bb14570
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-09-27 23:32:58 -0300

    update examples date manipulation

--- 5-JavaScript/class-04-data-manipulation/3-date.js
+++ 5-JavaScript/class-04-data-manipulation/3-date.js
@@ -1,61 +1,55 @@
-// Date Manipulation
+// Date Manipulation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
 
 // new: left-hand-side expression
 let dateNow = new Date();
-let date1 = new Date("2022-03-01 12:03:19");
-let date2 = new Date("2022-04-01 12:03:19");
+let date1 = new Date("2023-09-27 12:03:19");
+let date2 = new Date("2023-09-29 12:03:19");
 
-// document.write(dateNow);
-// let hour = dateNow.getHours();
-// let minutes = dateNow.getMinutes();
-// let seconds = dateNow.getSeconds();
-// let milliseconds = dateNow.getMilliseconds();
+console.log(Math.abs(date1 - date2) / 1000 / 60 / 60 / 24);
 
-document.write(
-  `${String(hour).padStart(2, "0")}:${String(minutes).padStart(2, "0")}:${String(
-    seconds
-  ).padStart(2, "0")}:${String(milliseconds).padStart(3, "0")}`
-);
+console.log(dateNow);
+console.log(date1);
 
-date1.setHours(date1.getHours() - 3);
-date1.setMinutes(date1.getMinutes() + 60);
-date1.setSeconds(date1.getSeconds() + 60);
-
-document.write(date1.toLocaleTimeString("pt-BR"));
+console.log(date2.toLocaleString("pt-BR"));
+console.log(date2.toLocaleDateString("pt-BR"));
+console.log(date2.toLocaleTimeString("pt-BR"));
 
 let day = dateNow.getDay();
-let date = dateNow.getDate();
-let month = dateNow.getMonth() + 1;
+let date = String(dateNow.getDate()).padStart(2, "0");
+let month = String(dateNow.getMonth()).padStart(2, "0");
 let year = dateNow.getFullYear();
-let minutes = date1.getMinutes();
-let seconds = date1.getSeconds();
 
-document.write(day + 1); // 0 - 6
-document.write(date);
-document.write(month + 1); // 0 - 11
-document.write(year);
+console.log(day); // 0 - 6
+console.log(date);
+console.log(month); // 0 - 11
+console.log(year);
 
-document.write(
-  `${String(date).padStart(2, "0")}/${String(month).padStart(2, "0")}/${year}`
-);
+console.log(`${date}/${month}/${year}`);
 
 dateNow.setDate(dateNow.getDate() + 30);
 dateNow.setMonth(dateNow.getMonth() + 2);
-dateNow.setFullYear(dateNow.getFullYear() + 30);
+dateNow.setFullYear(dateNow.getFullYear() + 3);
 
-document.write(
+console.log(dateNow.toLocaleDateString("pt-BR"));
+
+let hour = String(dateNow.getHours()).padStart(2, "0");
+let minutes = String(dateNow.getMinutes()).padStart(2, "0");
+let seconds = String(dateNow.getSeconds()).padStart(2, "0");
+let milliseconds = String(dateNow.getMilliseconds()).padStart(3, "0");
+
+console.log(`${hour}:${minutes}:${seconds}:${milliseconds}`);
+
+dateNow.setHours(dateNow.getHours() + 1);
+dateNow.setMinutes(dateNow.getMinutes() + 60);
+dateNow.setSeconds(dateNow.getSeconds() + 60);
+
+console.log(dateNow.toLocaleTimeString("pt-BR"));
+
+console.log(
   dateNow.toLocaleDateString("pt-BR", {
+    weekday: "long",
     day: "2-digit",
-    month: "2-digit",
-    year: "2-digit",
+    month: "long",
+    year: "numeric",
   })
 );
-
-//narrow, short, long
-const formatOptions = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
-document.write(Intl.DateTimeFormat("pt-BR", formatOptions).format(dateNow));
-
-document.write(dateNow.toLocaleString("pt-BR"));
-
-//   s      m    h    d
-document.write(Math.abs(date1 - date2) / 1000 / 60 / 60 / 24);

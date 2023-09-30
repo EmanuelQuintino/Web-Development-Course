3c976cd5df997b1317ac37d7ba3754cfe563df6c
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-09-30 20:23:55 -0300

    add example throw error divide number

--- 5-JavaScript/class-05-conditionals/3-try-catch.js
+++ 5-JavaScript/class-05-conditionals/3-try-catch.js
@@ -3,9 +3,20 @@ const runMyAplication = () => "Running Function...";
 try {
   console.log(runMyAplication());
 } catch (error) {
-  console.log(`My Test: ${error}`);
+  console.error(`My Test: ${error}`);
 } finally {
   console.log(`Sempre será executado!`);
 }
 
 document.write("Rodando aplicação!");
+
+function divideNumbers(num1, num2 = 1) {
+  if (num2 === 0) throw new Error("Divisor não pode ser 0!");
+  return num1 / num2;
+}
+
+try {
+  console.log(divideNumbers(4, 0));
+} catch (error) {
+  console.error(error);
+}

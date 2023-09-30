9dc02ebf0a9e5fa44ecf4e460fecc2eb3aa58e33
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-09-29 23:52:44 -0300

    add switch case and try catch files

---
+++ 5-JavaScript/class-05-conditionals/3-try-catch.js
@@ -0,0 +1,12 @@
+const runMyAplication = () => "Running Function..."
+
+try {
+    console.log(runMyAplication())
+} catch (error) {
+    console.log(`My Test: ${error}`)
+}
+finally {
+    console.log(`Sempre será executado!`)
+}
+
+document.write("Rodando aplicação!")

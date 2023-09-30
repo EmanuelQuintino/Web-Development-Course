71aeedc5ef1e6d104e7919ae948ac9468a9ac62b
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-09-29 23:47:22 -0300

    add file if else

--- 5-JavaScript/class-05-conditionals/1-if-else.js
+++ 5-JavaScript/class-05-conditionals/1-if-else.js
@@ -3,7 +3,7 @@ const userAge = 30
 if (userAge >= 18) {
     console.log("Pode dirigir")
 } else {
-    console.log("Pode dirigir")
+    console.log("Não pode dirigir")
 }
 
 if(age >= 18) {
@@ -42,82 +42,3 @@ else {
         window.alert('Muito caro, estou fora!')
     }
 }
-
-
->>>>>>>> Switch <<<<<<<<<
-
-let weekDay = 9
-
-switch (weekDay) {
-    case 1:
-        console.log("Domingo")
-        break;
-    case 2:
-        console.log("Segunda")
-        break;
-    case 3:
-        console.log("Terça")
-        break;
-    case 4:
-        console.log("Quarta")
-        break;
-    case 5:
-        console.log("Quinta")
-        break;
-    case 6:
-        console.log("Sexta")
-        break;
-    case 7:
-        console.log("Sábado")
-        break;
-    default:
-        console.log("Dia inválido!")
-        break;
-}
-
-
-let option = window.prompt(`
-    Digite a opção desejada:
-
-    1: Consultoria
-    2: Atendimento
-    3: Encerar`)
-
-switch (option){
-    case '1':
-        window.alert(`
-        Consultorias em:
-
-        - Finanças
-        - Vendas
-        - Markting`)
-        break;
-    case '2':
-        window.alert('Vamos passar para um de nossos atendentes!')
-        break;
-    case '3':
-        window.alert('Obrigado pela sua visita, volte sempre!')
-        break;
-    case null:
-        window.alert('Programa ecerrado!')
-        break;
-    default:
-        window.alert('Digite uma opção válida!')
-        break
-}
-
-
->>>>>>> Throw, Try, Catch and Finally <<<<<<<<<
-
-const runMyAplication = () => "Running Function..."
-
-try {
-    console.log(runMyAplication())
-} catch (error) {
-    console.log(`My Test: ${error}`)
-}
-finally {
-    console.log(`Sempre será executado!`)
-}
-
-document.write("Rodando aplicação!")

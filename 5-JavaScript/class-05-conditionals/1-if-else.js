6a47281ec64c24d56529fb081daae300d7f5ad39
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-09-29 23:46:21 -0300

    update challenge student average

---
+++ 5-JavaScript/class-05-conditionals/1-if-else.js
@@ -0,0 +1,123 @@
+const userAge = 30
+
+if (userAge >= 18) {
+    console.log("Pode dirigir")
+} else {
+    console.log("Pode dirigir")
+}
+
+if(age >= 18) {
+    console.log("Maior de idade!")
+} else {
+    console.log("Menor de idade!")
+}
+
+if(age >= 16) {
+    if(age >=18 && age <=70) {
+        console.log("Voto obrigatório!")
+    }
+    else {
+        console.log("Voto facultativo!")
+    }
+}
+else {
+    console.log("Não pode votar!")
+}
+
+let money = window.prompt('Quanto você tem?')
+let price = window.prompt('Quanto custa a blusa?')
+
+if (money > price){
+        window.alert('Está com preço bom, vou comprar!')
+    }
+else if (money == price){
+    window.alert('Vou ficar sem dinheiro mas compro a blusa!')
+}
+else {
+    let resp = window.prompt('Pode pegar dinheiro emprestado? (S/N)')
+    if (resp.toLowerCase() == 's'){
+        window.alert('Me individei mas comprei a blusa!')
+    }
+    else{
+        window.alert('Muito caro, estou fora!')
+    }
+}
+
+
+>>>>>>>> Switch <<<<<<<<<
+
+let weekDay = 9
+
+switch (weekDay) {
+    case 1:
+        console.log("Domingo")
+        break;
+    case 2:
+        console.log("Segunda")
+        break;
+    case 3:
+        console.log("Terça")
+        break;
+    case 4:
+        console.log("Quarta")
+        break;
+    case 5:
+        console.log("Quinta")
+        break;
+    case 6:
+        console.log("Sexta")
+        break;
+    case 7:
+        console.log("Sábado")
+        break;
+    default:
+        console.log("Dia inválido!")
+        break;
+}
+
+
+let option = window.prompt(`
+    Digite a opção desejada:
+
+    1: Consultoria
+    2: Atendimento
+    3: Encerar`)
+
+switch (option){
+    case '1':
+        window.alert(`
+        Consultorias em:
+
+        - Finanças
+        - Vendas
+        - Markting`)
+        break;
+    case '2':
+        window.alert('Vamos passar para um de nossos atendentes!')
+        break;
+    case '3':
+        window.alert('Obrigado pela sua visita, volte sempre!')
+        break;
+    case null:
+        window.alert('Programa ecerrado!')
+        break;
+    default:
+        window.alert('Digite uma opção válida!')
+        break
+}
+
+
+>>>>>>> Throw, Try, Catch and Finally <<<<<<<<<
+
+const runMyAplication = () => "Running Function..."
+
+try {
+    console.log(runMyAplication())
+} catch (error) {
+    console.log(`My Test: ${error}`)
+}
+finally {
+    console.log(`Sempre será executado!`)
+}
+
+document.write("Rodando aplicação!")
--- 5-JavaScript/class-05-conditionals/challenges/average-calculate/script.js
+++ 5-JavaScript/class-05-conditionals/challenges/average-calculate/script.js
@@ -1,24 +1,18 @@
-// Crie um algoritmo que solicite o Nome e Notas 1 e 2 do aluno
-// Crie uma função para calcular a sua média
-// Depois apresente o nome do aluno sua media e sua situação
-// O aluno será aprovado somente SE ele teve média maior ou igual 7.
+// Crie uma função para calcular média do aluno
+// A média da escola é 7 e ela realiza duas provas
+// Apresente a média e a situação se aprovado ou reprovado
 
-function studantAverage(nota1, nota2) {
-    return (nota1 + nota2) / 2
+function studentAverage(grade1, grade2) {
+  return (grade1 + grade2) / 2;
 }
 
-const nameStudant = prompt("Nome: ")
-const nota1 = Number(prompt("Nota 1: "))
-const nota2 = Number(prompt("Nota 2: "))
+const grade1 = Number(prompt("Nota 1: "));
+const grade2 = Number(prompt("Nota 2: "));
 
-const average = studantAverage(nota1, nota2)
-let situation = ''
+const average = studentAverage(grade1, grade2);
 
 if (average >= 7) {
-    situation = 'aprovado(a)!'
+  alert(`O aluno teve média de ${average} e foi aprovado`);
+} else {
+  alert(`O aluno teve média de ${average} e foi reprovado`);
 }
-else {
-    situation = 'reprovado(a)!'
-}
-
-alert(`${nameStudant} teve média de ${average} e foi ${situation}`)

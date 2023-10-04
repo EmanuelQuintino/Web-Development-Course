fcedc6dd514b36d00588c75cc233d2d6a833d024
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-10-04 13:28:52 -0300

    refactor challenge convert temperature

--- 5-JavaScript/class-05-conditionals/challenges/3-convert-temperature.js
+++ 5-JavaScript/class-05-conditionals/challenges/3-convert-temperature.js
@@ -7,25 +7,35 @@
 // F = C * 9/5 + 32
 
 function degreeConvert(degree) {
-  let auxDegree = String(degree).split(" ").join("");
-  let arrayDegree = Array.from(auxDegree);
-  let scaleDegree = arrayDegree.pop();
-  let newDegree = Number(arrayDegree.join(""));
+  const auxDegree = String(degree).split(" ").join("");
+  const arrayDegree = Array.from(auxDegree);
+  const scaleDegree = arrayDegree.pop().toUpperCase();
+  const newDegree = Number(arrayDegree.join(""));
 
   if (isNaN(newDegree)) {
     throw new Error("Valor inválido! Insira conforme exemplo: 10F ou 20C.");
   }
-  if (scaleDegree.toUpperCase() == "C") {
+
+  function convertToFahrenheit() {
     return ((newDegree * 9) / 5 + 32).toLocaleString("pt-BR") + "F";
-  } else if (scaleDegree.toUpperCase() == "F") {
+  }
+
+  function convertToCelsius() {
     return (((newDegree - 32) * 5) / 9).toLocaleString("pt-BR") + "C";
-  } else {
-    throw new Error("Escala não identificada, insira 'C' ou 'F': Ex: 10F ou 20C.");
+  }
+
+  switch (scaleDegree) {
+    case "C":
+      return convertToFahrenheit();
+    case "F":
+      return convertToCelsius();
+    default:
+      throw new Error("Escala não identificada, insira 'C' ou 'F': Ex: 10F ou 20C.");
   }
 }
 
 try {
-  let temperature = "10C";
+  const temperature = "10C";
   console.log(`${temperature.toUpperCase()} =  ${degreeConvert(temperature)}`);
 } catch (error) {
   console.error(error);

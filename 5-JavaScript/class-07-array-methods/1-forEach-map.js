0aee23d76d7cfde1d76e4bd70878e5f9a63f6227
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-10-11 22:56:58 -0300

    add example map retangle calculate area

--- 5-JavaScript/class-07-array-methods/1-forEach-map.js
+++ 5-JavaScript/class-07-array-methods/1-forEach-map.js
@@ -19,9 +19,21 @@ console.log(arrayForEach);
 console.log(array);
 console.log(arrayMap);
 
-const dollar = [1, 2, 3000, 40, 50];
+const arrayDollar = [1, 2, 3000, 40, 50];
 
-const realConverted = dollar.map((value) => value * 5.05);
+const realConverted = arrayDollar.map((dollar) => dollar * 5.05);
 
-console.log(dollar);
+console.log(arrayDollar);
 console.log(realConverted);
+
+const rectangles = [
+  { width: 4, height: 5 },
+  { width: 7, height: 3 },
+  { width: 2, height: 8 },
+];
+
+const calculatedAreas = rectangles.map((rectangle) => {
+  return { area: rectangle.width * rectangle.height };
+});
+
+console.log(calculatedAreas);

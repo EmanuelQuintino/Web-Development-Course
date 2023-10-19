61fb20844eaf960ac430afd3d0fc9f6559c80349
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-10-19 09:37:36 -0300

    update class find filter

--- 5-JavaScript/class-07-array-methods/2-find-filter.js
+++ 5-JavaScript/class-07-array-methods/2-find-filter.js
@@ -1,31 +1,22 @@
 // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 
-const averageStudants = [
-  { id: 1, name: "João", average: 4, birth: 1990 },
+const studants = [
+  { id: 1, name: "João", average: 6, birth: 2005 },
   { id: 2, name: "Thiago", average: 6, birth: 2002 },
   { id: 3, name: "Jonas", average: 5, birth: 1998 },
   { id: 4, name: "Rosa", average: 9, birth: 2004 },
   { id: 5, name: "Amanda", average: 8, birth: 1992 },
-  { id: 6, name: "Pedro", average: 7, birth: 1993 },
+  { id: 6, name: "Pedro", average: 7, birth: 1998 },
 ];
 
-console.log(averageStudants.find((studant) => yearCurrent - studant.birth >= 30));
-console.log(
-  averageStudants.find((studant) => studant.name.toLocaleLowerCase() == "amanda")
-);
-console.log(averageStudants.find((studant) => studant.id == 5));
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
 
-console.log(real.filter((value) => value < 40));
+console.log(studants.find((student) => student.id === 3));
+console.log(studants.find((studant) => studant.name === "Amanda"));
+console.log(studants.find((studant) => studant.average >= 7));
 
-console.table(averageStudants.filter((studant) => studant.average >= 7));
-console.table(averageStudants.filter((studant) => studant.birth >= 2000));
-
-let yearCurrent = new Date().getFullYear();
-console.log(yearCurrent);
-
-console.table(averageStudants.filter((studant) => yearCurrent - studant.birth >= 30));
-console.table(
-  averageStudants.filter(
-    (studant) => Array.from(studant.name)[0].toLocaleLowerCase() == "j"
-  )
-);
+console.table(studants.filter((studant) => studant.average >= 7));
+console.table(studants.filter((studant) => studant.birth >= 2000));
+console.table(studants.filter((studant) => currentYear - studant.birth <= 18));
+console.table(studants.filter((studant) => studant.name.includes("d")));

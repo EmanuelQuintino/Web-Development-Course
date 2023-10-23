47bd0736ababc8fe60f11d3ed9338830b9e655a2
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-10-23 18:37:10 -0300

    update examples reduce

--- 5-JavaScript/class-07-array-methods/3-reduce.js
+++ 5-JavaScript/class-07-array-methods/3-reduce.js
@@ -1,10 +1,20 @@
 // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
 
+const arrayNumbers = [2, 4, 7, 2, 3, 5];
+
+const sumNumbers = arrayNumbers.reduce((a, b) => a + b, 0);
+console.log(sumNumbers);
+
 let initial = 200;
-const sumReal = real.reduce((a, b) => a + b, initial);
-console.log(sumReal);
 
-const sumConvertedDollar = convertDollar.reduce((a, b) => a + b);
-console.log(sumConvertedDollar);
+const shoppingCart = [
+  { product: "Arroz", quantity: 2, price: 10 },
+  { product: "Feijão", quantity: 3, price: 20 },
+  { product: "Açúcar", quantity: 4, price: 30 },
+];
 
-console.log(real.map((value) => value * 5.36).reduce((a, b) => a + b));
+const sumShoppingCart = shoppingCart.reduce((prevValue, currentValue) => {
+  return prevValue + currentValue.quantity * currentValue.price;
+}, 0);
+
+console.log(sumShoppingCart);

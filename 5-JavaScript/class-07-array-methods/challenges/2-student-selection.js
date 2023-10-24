7c5a345db2b83ffe3720d4ec3f4e7d859b30b2cf
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-10-24 00:09:56 -0300

    update challenge array methods

---
+++ 5-JavaScript/class-07-array-methods/challenges/2-student-selection.js
@@ -0,0 +1,34 @@
+// Uma escola precisa selecionar os melhores alunos para estágio
+// Use os métodos de Array para percorer lista de alunos e calcular médias
+// O critério será por média >= 7 e as faltas menores que 5 para filtrar alunos
+
+const students = [
+  { name: "Aluno A", grades: [7, 7, 6], absences: 3 },
+  { name: "Aluno B", grades: [6, 8, 7], absences: 4 },
+  { name: "Aluno C", grades: [9, 8, 10], absences: 6 },
+  { name: "Aluno D", grades: [9, 7, 8], absences: 2 },
+  { name: "Aluno E", grades: [8, 7, 5], absences: 7 },
+];
+
+const studentsAverage = students.map((student) => {
+  const average =
+    student.grades.reduce((prev, grade) => prev + grade, 0) / student.grades.length;
+
+  return {
+    name: student.name,
+    average: average,
+    absences: student.absences,
+  };
+});
+
+const selectedStudents = studentsAverage.filter((student) => {
+  if (student.average >= 7 && student.absences < 5) {
+    return {
+      name: student.name,
+      average: student.average,
+      absences: student.absences,
+    };
+  }
+});
+
+console.table(selectedStudents);

574f4b81bcf4677b16de63cbea38731f331ab3b8
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-10-25 11:43:42 -0300

    update name function arraySumMaxMin

--- 5-JavaScript/class-07-array-methods/challenges/2-array-sumMinMax.js
+++ 5-JavaScript/class-07-array-methods/challenges/2-array-sumMinMax.js
@@ -1,7 +1,7 @@
 // Faça uma função que receba um array de números
 // Retorne um objeto com a soma dos 3 maiores e 3 menos números
 
-function sumMaxMin(arrayNumbers) {
+function arraySumMaxMin(arrayNumbers) {
   const orderArray = arrayNumbers.sort((a, b) => a - b);
   const sumMax = orderArray.slice(-3).reduce((a, b) => a + b);
   const sumMin = orderArray.slice(0, 3).reduce((a, b) => a + b);
@@ -10,4 +10,4 @@ function sumMaxMin(arrayNumbers) {
 
 const arrayNumbers = [5, 4, 3, 2, 1];
 
-console.log(sumMaxMin(arrayNumbers));
+console.log(arraySumMaxMin(arrayNumbers));

dd03b145a7c1b5d23e5c14d80187850055686add
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-10-27 22:54:48 -0300

    update javascript evaluation

--- 5-JavaScript/evaluation-javascript/evaluation.js
+++ 5-JavaScript/evaluation-javascript/evaluation.js
@@ -1,13 +1,15 @@
-// Question 1 (Data Types):
+// Question 1 (Data Types)
+// Declare as seguntes variáveis e atribua os valores por tipo: Sting, Number, Boolean, Function, Array, Object
 
 let number = 1;
 let sting = "1";
 let boolean = true;
-let myFunction = () => {};
-let array = [];
-let object = {};
+let myFunction = function () {};
+let array = [1, "A", true, [{}, {}], () => {}];
+let object = { key: "value" };
 
-// Question 2 (Operators):
+// Question 2 (Operators)
+// Imprima as operações de soma, subtração, multiplicação, divisão, resto da divisão e potenciação de 12 e 3 e raiz quadrada de 144
 
 console.log(12 + 3);
 console.log(12 - 3);
@@ -16,8 +18,10 @@ console.log(12 / 3);
 console.log(12 % 3);
 console.log(12 ** 3);
 console.log(144 ** (1 / 2));
+console.log(Math.sqrt(144));
 
-// Question 3 (Functions):
+// Question 3 (Functions)
+// Crie e execute uma função de nome 'test" que retorne "JavaScript" concatenado com um valor passado como argumento e imprima seu retorno
 
 function test(value) {
   return "JavaScript " + value;
@@ -25,15 +29,18 @@ function test(value) {
 
 console.log(test("test"));
 
-// Question 4 (Data Manipulation):
+// Question 4 (Data Manipulation)
+// Usando os métodos FIFO e LIFO, crie um array com 3 valos e posteriormente insira mais um valor no final e substitua o do início por outro
 
 const list = ["A", "B", "C"];
 list.push("D");
 list.shift();
 list.unshift("E");
+list[0] = "test";
 console.log(list);
 
-// Question 5 (for):
+// Question 5 (for)
+// Liste todos os valores do array criado na questão anterior ultilizando o for
 
 for (let index = 0; index < list.length; index++) {
   console.log(list[index]);
@@ -43,33 +50,53 @@ for (const value of list) {
   console.log(value);
 }
 
-// Question 6 (while):
+// Question 6 (while)
+// Escreva os números de 0 a 100 sem os que pertencem à familía do 40, do 50 e do 60
 
 let count = 0;
-while (count <= 500) {
-  if (count < 200 || count >= 400) {
+while (count <= 100) {
+  if (count < 40 || count >= 70) {
     console.log(count);
   }
   count++;
 }
 
-// Question 7 (if):
+count = 0;
+while (count <= 100) {
+  if (count >= 40 && count < 70) {
+    count++;
+    continue;
+  }
+  console.log(count);
+  count++;
+}
+
+// Question 7 (if)
+// Crie uma lógica para verificar se o motorista foi multado
+// A infração pode ser por velocidade alta (quando ultrapassar a máxima)
+// E por velocidade baixa ( quando estiver inferior à metade da máxima )
+// A velocidade da pista é de 60km/h
 
 const speed = 75;
 const maxSpeed = 60;
 
-if (speed > maxSpeed || speed < maxSpeed / 2) {
-  situation = "multado!";
+if (speed > maxSpeed) {
+  console.log("Multado por excesso de velocidade!");
+} else if (speed < maxSpeed / 2) {
+  console.log("Multado por baixa velocidade!");
 } else {
-  situation = "não multado!";
+  console.log("Não multado!");
 }
 
-console.log(`Motorista ${situation}`);
-
-// Question 8 (array methods):
+// Question 8 (array methods)
+// Uma empresa vai dar 10% de aumento para seus funcionários
+// Abaixo tem a lista com todos os salários atuais de cada um deles
+// Apresente os novos salários e o novo total em folha dessa empresa
 
 const salary = [2000, 2800, 3000, 4000, 2200, 7500, 3650, 8000, 2000, 5000];
-const newSalary = salary.map((salary) => (salary * 10) / 100 + salary);
-const sumNewSalary = newSalary.reduce((a, b) => a + b);
 
+const newSalary = salary.map((salary) => salary * 1.1);
+console.log(newSalary);
+
+const sumNewSalary = newSalary.reduce((prev, curr) => prev + curr);
 console.log(sumNewSalary.toLocaleString("pt-br", { style: "currency", currency: "brl" }));

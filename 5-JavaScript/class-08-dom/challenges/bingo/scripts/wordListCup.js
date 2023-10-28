a686ab530e2e64f19deebfc173288c7fbf720951
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-10-28 16:54:01 -0300

    remove project bingo

--- 5-JavaScript/class-08-dom/challenges/bingo/scripts/wordListCup.js
+++
@@ -1,7 +0,0 @@
-export const wordList = [
-    {number: 1, word: "Neymar", tips: "Nome do camisa 10 da seleção brasileira"},
-    {number: 2, word: "Catar", tips: "País cede da copa do mundo de 2022"},
-    {number: 3, word: "França", tips: "Vencedor da copa de 2018"},
-    {number: 4, word: "2002", tips: "Ano que o Brasil ganhou a quinta copa"},
-    {number: 5, word: "Alemanha", tips: "País que o Brasil venceu na final de 2002"},
-]

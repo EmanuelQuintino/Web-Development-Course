563b9cd7fbbd52af042c251675164d50bfb2ef45
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-11-04 12:30:04 -0300

    add reference class oop

---
+++ 5-JavaScript/extra-classes/oop/script.js
@@ -0,0 +1,45 @@
+// Reference: https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object-oriented_programming
+
+class Mamifero {
+  constructor() {
+    this.nascer();
+  }
+
+  nascer() {
+    console.log("Nasceu!");
+    this.tomarLeite();
+  }
+
+  tomarLeite() {
+    console.log(`Tomou leite!`);
+  }
+}
+
+class Terrestre extends Mamifero {
+  constructor() {
+    super();
+  }
+
+  locomocao() {
+    console.log("andar");
+  }
+}
+
+class Felino extends Terrestre {
+  constructor() {
+    super();
+  }
+
+  locomocao() {
+    console.log(`Andar e Correr`);
+  }
+}
+
+const mamifero = new Mamifero();
+// mamifero.tomarLeite()
+
+const macaco = new Terrestre();
+// macaco.locomocao()
+
+const leao = new Felino();
+leao.locomocao();

d5e7e70d0981914cb39e08b5ff1f1cae9a4b5f25
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-11-14 17:03:11 -0300

    add event loop reference link

--- 5-JavaScript/extra-classes/event-loop/script.js
+++ 5-JavaScript/extra-classes/event-loop/script.js
@@ -1,34 +1,32 @@
+// Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Event_loop
+
 // Call Stack
 console.log(1);
 
 // Call Stack
 // for (let index = 0; index < 200000; index++) {
 //     console.log(index);
-// }  
-
+// }
 
 // Microtask
 // queueMicrotask(() => {
 //   console.log(2);
 // });
 
-
 // Tasks
 setTimeout(() => {
   console.log(3);
 }, 2000);
-
 
 // Microtask
 // Promise.resolve(true).then(() => {
 //   console.log(4);
 // });
 
-
 function sum(a, b) {
   queueMicrotask(() => {
     console.log(6);
-  })
+  });
   return a + b;
 }
 

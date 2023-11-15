405c3b83b5832d2279291275e1b27a96688d987f
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-11-14 23:14:40 -0300

    add example closures

---
+++ 5-JavaScript/class-09-es6-features/4-closures.js
@@ -0,0 +1,4 @@
+import { incrementNum } from "./scripts/closure.js";
+
+let num = 20;
+console.log(incrementNum());
--- 5-JavaScript/extra-classes/closures/script.js
+++
@@ -1,4 +0,0 @@
-import { incrementNum } from "./closure.js";
-
-let num = 20;
-console.log(incrementNum());

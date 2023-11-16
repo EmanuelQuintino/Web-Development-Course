15257320bcb3c481e602170482d67d1f3bf4504c
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-11-16 08:59:02 -0300

    add class pokemon example

--- 5-JavaScript/class-11-oop/1-class.js
+++ 5-JavaScript/class-11-oop/1-class.js
@@ -1,20 +1,41 @@
 // Reference: https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object-oriented_programming
 
-class Person {
-  constructor(name, age) {
+class Pokemon {
+  constructor(name, level) {
     this.name = name;
-    this.age = age;
-    this.canDrive = age >= 18;
-    this.status = () => {
-      if (this.canDrive) {
-        return "Pode dirigir";
-      } else {
-        return "Não pode dirigir";
-      }
-    };
+    this.level = level;
+    this.health = 100;
+  }
+
+  attack() {
+    console.log(`${this.name} atacou!`);
+  }
+
+  damage(damage, effective) {
+    if (effective) {
+      let damageEffective = damage * 1.3;
+      this.health -= damageEffective;
+      console.log(`${this.name} sofreu ${damageEffective} de dano efetivo!`);
+    } else {
+      this.health -= damage;
+      console.log(`${this.name} sofreu ${damage} de dano!`);
+    }
   }
 }
 
-const person1 = new Person("Emanuel", 30);
-// console.log(person1.status());
-// console.log(typeof person1);
+const charmander = new Pokemon("Charmander", 4);
+const squirtle = new Pokemon("Squirtle", 5);
+const bulbasaur = new Pokemon("Bulbasaur", 6);
+
+console.log(charmander.name);
+console.log(squirtle.level);
+
+bulbasaur.attack();
+
+let effective = false;
+squirtle.damage(30, effective);
+console.log(squirtle.health);
+
+effective = true;
+squirtle.damage(30, effective);
+console.log(squirtle.health);

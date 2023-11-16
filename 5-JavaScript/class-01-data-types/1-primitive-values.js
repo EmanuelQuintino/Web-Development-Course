8556f20e8224eb92cb9fab40aa214a7e5b4f1254
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-11-16 18:10:20 -0300

    update class primitive values

--- 5-JavaScript/class-01-data-types/1-primitive-values.js
+++ 5-JavaScript/class-01-data-types/1-primitive-values.js
@@ -1,33 +1,38 @@
-// Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
+// Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_str uctures
 
 // Primitive Values : String, Number, Boolean...
 
 // let 123variable;
 // let my-variable;
 // let my variable;
-// let my$variable;
 // let my#variable;
-// let my-variável;
 // let if;
 // let break;
 
 // String
-let namePerson = "Emanuel";
-let age = 29;
+let studentName = "Emanuel";
+let age = "30";
 
-console.log(namePerson);
+console.log(studentName);
 console.log(typeof age);
 
 // Number: integer, float, NaN, Infinity
-console.log(typeof 32.2);
+console.log(typeof 32);
+console.log(typeof 32.5);
 console.log(typeof (32 + 5));
 console.log(typeof 15 + "Emanuel");
 console.log(typeof NaN);
+console.log(typeof Infinity);
+console.log(2 / 0);
+
+let number;
+console.log(number);
 
 // Boolean: true or false
+console.log(typeof true);
 console.log(typeof (10 == 20));
 console.log(20 == 20);
 console.log(20 == "Emanuel");
 console.log(20 != "Emanuel");
 console.log(11 == "11");
-console.log(11 === "11"); //cuidado porque são tipos diferentes de dados
+console.log(11 === "11");

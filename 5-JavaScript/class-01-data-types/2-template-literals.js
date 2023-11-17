bfc458f0aea2f869f79a59924b470201d41b6169
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-11-17 17:15:17 -0300

    update class template literals

--- 5-JavaScript/class-01-data-types/2-template-literals.js
+++ 5-JavaScript/class-01-data-types/2-template-literals.js
@@ -1,21 +1,30 @@
 // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
 
+console.log("Emanuel");
+console.log("Emanuel");
+console.log(`Emanuel`);
+
+console.log("I can't play outside");
+console.log('Emanuel disse "Estudem JavaScript"');
+
+let course = "Desenvolvimento Web";
+console.log("Turma de " + course); // concatenation
+
 let num1 = "10";
 let num2 = 20;
 
-num1 = Number(num1); // Type Conversion (Typecasting)
-// Number() (função construtora para converter um dado em número)
+num1 = Number(num1); // type conversion (typecasting)
 
-console.log(num1 + num2); // Type Coersion
+console.log(num1 + num2); // type coersion
 console.log(Number("10") + 20);
 
-// Concatenation (grouping operator)
+// grouping operator
 console.log("A soma é: " + num1 + num2);
 console.log("A soma é: " + (num1 + num2));
 
-// Interpolation
+// interpolation
 console.log("A soma de " + num1 + " e de " + num2 + " é igual a " + (num1 + num2));
 
-// Template Literals (Strings)
-let addition = num1 + num2;
-console.log(`A soma de ${num1} e de ${num2} é igual a ${addition}`);
+// template literals (strings)
+const sumNumbers = num1 + num2;
+console.log(`A soma de ${num1} e de ${num2} é igual a ${sumNumbers}`);

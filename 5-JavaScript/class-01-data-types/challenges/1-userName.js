d22a72278544087aeb9a2067934ecb7c375447b4
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-11-24 14:55:34 -0300

    update name file userName

---
+++ 5-JavaScript/class-01-data-types/challenges/1-userName.js
@@ -0,0 +1,4 @@
+// ⚠️ Desafio: Faça um programa que solicite (prompt) o nome do usuário e dê as boas vindas (alert): Ex: Olá, "nome", seja bem-vindo!
+
+let userName = prompt("Digite seu nome: ");
+alert("Olá, " + userName + ", seja bem-vindo!");

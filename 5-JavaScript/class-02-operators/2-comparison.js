1f05939806dcd5afdd28a26cd968b72dc7706bad
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-11-27 15:58:23 -0300

    update examples class comparison

--- 5-JavaScript/class-02-operators/2-comparison.js
+++ 5-JavaScript/class-02-operators/2-comparison.js
@@ -6,11 +6,16 @@
 // ==   !=
 // ===  !==
 
+let num1 = 2;
+let num2 = 3;
+
 console.log(num1 > num2);
-console.log(num1 >= num2);
 console.log(num1 < num2);
-console.log(num1 <= num2);
+console.log(num1 >= 2);
+console.log(num2 <= 3);
 console.log(num1 == num2);
 console.log(num1 != num2);
-console.log(num1 === num2);
-console.log(num1 !== num2);
+console.log(num1 == "2");
+console.log(num1 === "2");
+console.log(num1 != "2");
+console.log(num1 !== "2");

e7c8af30ea994e4eb9098cc9cef9fb76d1f3d7db
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-11-28 16:02:31 -0300

    add examples logical operators

--- 5-JavaScript/class-02-operators/3-logical.js
+++ 5-JavaScript/class-02-operators/3-logical.js
@@ -8,12 +8,23 @@
 
 //Go to Walt Disney?
 
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
 let creditCard = true;
 let hasCredit = true;
 
+console.log(!true);
 console.log(!!creditCard);
 
-console.log(creditCard && hasCredit);
+console.log("Viagem: " + (creditCard && hasCredit && true));
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
 
 let saveMoney = true;
 let getLoan = true;

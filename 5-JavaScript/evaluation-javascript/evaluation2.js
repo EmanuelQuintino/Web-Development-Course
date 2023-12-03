b5c4dfc4036ae377311226aef9dba2dc865ce144
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-12-03 19:31:02 -0300

    add evaluation 2

---
+++ 5-JavaScript/evaluation-javascript/evaluation2.js
@@ -0,0 +1,67 @@
+// Question 1 (Data Types)
+console.log(10 == "10");
+
+// Question 2 (Data Types)
+console.log(2 + +"3");
+// a) error
+// b) 5
+// c) 23
+// d) N.R.A
+
+// Question 3 (Data Types)
+console.log(typeof 2 / 0);
+
+// a) error
+// b) infinity
+// c) number
+// d) N.R.A
+
+// Question 4 (scope variebles)
+
+console.log(num);
+
+{
+  var num = 2;
+}
+
+console.log(num);
+
+// a) undefined, 2
+// b) undefined, undefined
+// c) error
+// d) N.R.A
+
+// Question 5 (array)
+
+const filmList = ["Matrix", "Interestellar", "Shrek", "Get Out", "Split"];
+
+// a) console.log(filmList[1]);
+// b) console.log(filmList[2]);
+// c) console.log(filmList[3]);
+// d) N.R.A
+
+// Question 6 (Operators)
+
+let num1 = 2;
+
+console.log(num1++);
+console.log(++num1);
+console.log(num1++ + 3);
+console.log(num1);
+
+// a) 2, 4, 7, 5
+// b) 2, 4, 8, 8
+// c) 3, 4, 8, 8
+// d) error
+
+// Question 3 (Functions)
+
+// Question 4 (Data Manipulation)
+
+// Question 5 (for)
+
+// Question 6 (while)
+
+// Question 7 (if)
+
+// Question 8 (array methods)

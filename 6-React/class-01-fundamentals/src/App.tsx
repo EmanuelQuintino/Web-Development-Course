23567570cb03207ce58116c327092b9229160b36
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-12-04 15:31:52 -0300

    update class 01 fundamentals

---
+++ 6-React/class-01-fundamentals/src/App.tsx
@@ -0,0 +1,11 @@
+import imageReact from "./assets/react.svg";
+
+export function App() {
+  return (
+    <>
+      <h1>Hello World!!!</h1>
+      <img src="vite.svg" alt="" width={120} />
+      <img src={imageReact} alt="" width={120} />
+    </>
+  );
+}

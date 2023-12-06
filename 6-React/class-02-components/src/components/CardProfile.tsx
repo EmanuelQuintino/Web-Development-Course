7344120c45f0b668a16717e1651a647d9b3da541
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-12-06 18:35:03 -0300

    update example class components

--- 6-React/class-02-components/src/components/CardProfile.tsx
+++ 6-React/class-02-components/src/components/CardProfile.tsx
@@ -3,15 +3,16 @@ import "./style.css";
 type CardProfileProps = {
   gitHub: string;
   name: string;
-  age: number;
+  birth: number;
+  calcAge: (age: number) => number;
 };
 
-export function CardProfile({ gitHub, name, age }: CardProfileProps) {
+export function CardProfile({ gitHub, name, birth, calcAge }: CardProfileProps) {
   return (
     <div className="card">
       <img src={`https://github.com/${gitHub}.png`} alt="image-profile" />
       <strong>{name}</strong>
-      <p>Idade: {age}</p>
+      <p>Idade: {calcAge(birth)}</p>
     </div>
   );
 }
--- 6-React/class-02-components/src/pages/App.tsx
+++ 6-React/class-02-components/src/pages/App.tsx
@@ -1,17 +1,30 @@
 import { CardProfile } from "../components/CardProfile";
 
 const users = [
-  { id: 1, gitHub: "EmanuelQuintino", name: "Emanuel Quintino", age: 30 },
-  { id: 2, gitHub: "EmanuelQuintino", name: "Emanuel Quintino", age: 30 },
-  { id: 3, gitHub: "EmanuelQuintino", name: "Emanuel Quintino", age: 30 },
+  { id: 1, gitHub: "EmanuelQuintino", name: "Emanuel Quintino", birth: 1992 },
+  { id: 2, gitHub: "EmanuelQuintino", name: "Emanuel Quintino", birth: 1992 },
+  { id: 3, gitHub: "EmanuelQuintino", name: "Emanuel Quintino", birth: 1992 },
 ];
+
+function calcAge(birth: number) {
+  const currentYear = new Date().getFullYear();
+  return currentYear - birth;
+}
 
 export function App() {
   return (
     <>
       <h1 style={{ textAlign: "center" }}>Class Components</h1>
-      {users.map(({ id, gitHub, name, age }) => {
-        return <CardProfile gitHub={gitHub} name={name} age={age} key={id} />;
+      {users.map(({ id, gitHub, name, birth }) => {
+        return (
+          <CardProfile
+            gitHub={gitHub}
+            name={name}
+            birth={birth}
+            key={id}
+            calcAge={calcAge}
+          />
+        );
       })}
     </>
   );

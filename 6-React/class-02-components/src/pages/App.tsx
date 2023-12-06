e4d027a1246b12e1e2e6dd7e9651d4e8e4b1b8e9
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-12-06 14:12:59 -0300

    add typescript example

--- 6-React/class-02-components/src/pages/App.tsx
+++ 6-React/class-02-components/src/pages/App.tsx
@@ -1,12 +1,10 @@
 import { CardProfile } from "../components/CardProfile";
 
-function App() {
+export function App() {
   return (
     <>
-      <h1 style={{textAlign: "center"}}>Class Components</h1>
+      <h1 style={{ textAlign: "center" }}>Class Components</h1>
       <CardProfile gitHub={"EmanuelQuintino"} name={"Emanuel Quintino"} age={30} />
     </>
   );
 }
-
-export default App;

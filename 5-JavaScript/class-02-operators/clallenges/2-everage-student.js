6622b9490d2b7ba695b48d3fd24e467e02696cad
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-12-07 18:01:42 -0300

    add challenge everage student

---
+++ 5-JavaScript/class-02-operators/clallenges/2-everage-student.js
@@ -0,0 +1,11 @@
+// Construa uma lógica receba as notas 1 e 2 do estudante
+// Imprima na tela (alert) sua média e diga sua situação
+// A média da escola é 7 para aprovação
+
+const grade1 = Number(prompt("Digite a primeira nota:"));
+const grade2 = Number(prompt("Digite a segunda nota:"));
+
+const studentAverage = (grade1 + grade2) / 2;
+const situation = studentAverage >= 7 ? "aprovado" : "reprovado";
+
+window.alert(`Média ${studentAverage} e está ${situation}!`);
--- 5-JavaScript/class-02-operators/clallenges/2-number-evenOdd.js
+++
@@ -1,8 +0,0 @@
-// Construa uma lógica para dizer se um número é par ou é ímpar
-// Receba o número desejado do usuário via promp
-// Escreve na tela o número e se ele é par ou é ímpar
-
-const numUser = Number(prompt("Digite um número para saber se é par ou é ímpar: "));
-const result = numUser % 2 == 0 ? "Par" : "Ímpar";
-
-window.alert(`O número ${numUser} é ${result}`);

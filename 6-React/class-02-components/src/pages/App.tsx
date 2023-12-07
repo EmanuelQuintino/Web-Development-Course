c6cc58a9c516628b0fcb779d31e48e0ca9f4d3d5
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-12-07 10:59:00 -0300

    update class example components

--- 6-React/class-02-components/src/pages/App.tsx
+++ 6-React/class-02-components/src/pages/App.tsx
@@ -1,11 +1,5 @@
 import { CardProfile } from "../components/CardProfile";
 
-const users = [
-  { id: 1, gitHub: "EmanuelQuintino", name: "Emanuel Quintino", birth: 1992 },
-  { id: 2, gitHub: "EmanuelQuintino", name: "Emanuel Quintino", birth: 1992 },
-  { id: 3, gitHub: "EmanuelQuintino", name: "Emanuel Quintino", birth: 1992 },
-];
-
 function calcAge(birth: number) {
   const currentYear = new Date().getFullYear();
   return currentYear - birth;
@@ -15,17 +9,19 @@ export function App() {
   return (
     <>
       <h1 style={{ textAlign: "center" }}>Class Components</h1>
-      {users.map(({ id, gitHub, name, birth }) => {
-        return (
-          <CardProfile
-            gitHub={gitHub}
-            name={name}
-            birth={birth}
-            key={id}
-            calcAge={calcAge}
-          />
-        );
-      })}
+      <CardProfile
+        gitHub={"EmanuelQuintino"}
+        name={"Emanuel Quintino"}
+        birth={1992}
+        calcAge={calcAge}
+      />
+
+      <CardProfile
+        gitHub={"EmanuelQuintino"}
+        name={"Emanuel Quintino"}
+        birth={1992}
+        calcAge={calcAge}
+      />
     </>
   );
 }

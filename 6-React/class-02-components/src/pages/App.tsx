de2d3ef959e5a77d2686e3c4a8da80d8b4c5d4e1
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-12-08 19:22:56 -0300

    update class 02 examples

--- 6-React/class-02-components/src/pages/App.tsx
+++ 6-React/class-02-components/src/pages/App.tsx
@@ -1,20 +1,20 @@
 import { CardProfile } from "../components/CardProfile";
 
-function calcUserAge(birth: string) {
-  const dateNow = new Date().getTime();
-  const dateBirth = new Date(birth).getTime();
-  const userAge = Math.floor((dateNow - dateBirth) / (1000 * 60 * 60 * 24 * 365.25));
-  return userAge;
-}
-
-const dataAPI = [
-  { id: 1, name: "Emanuel", birth: "12-25-1992" },
-  { id: 2, name: "Éric", birth: "06-18-2004" },
-  { id: 3, name: "Jorge", birth: "01-23-1995" },
-  { id: 4, name: "Jonatas", birth: "12-01-2004" },
-];
-
 export function App() {
+  const dataAPI = [
+    { id: 1, name: "Emanuel", birth: "12-25-1992" },
+    { id: 2, name: "Éric", birth: "06-18-2004" },
+    { id: 3, name: "Jorge", birth: "01-23-1995" },
+    { id: 4, name: "Jonatas", birth: "12-01-2004" },
+  ];
+
+  function calcUserAge(birth: string) {
+    const dateNow = new Date().getTime();
+    const dateBirth = new Date(birth).getTime();
+    const userAge = Math.floor((dateNow - dateBirth) / (1000 * 60 * 60 * 24 * 365.25));
+    return userAge;
+  }
+
   return (
     <>
       <h1>Class 02 Components</h1>

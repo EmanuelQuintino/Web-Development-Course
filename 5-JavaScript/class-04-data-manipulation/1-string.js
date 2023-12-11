d5a6bc099be88c318f9b0c4731ffa5e30abc6dbc
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-12-11 16:12:46 -0300

    update class string manipulation

--- 5-JavaScript/class-04-data-manipulation/1-string.js
+++ 5-JavaScript/class-04-data-manipulation/1-string.js
@@ -1,6 +1,4 @@
-// String Manipulation
-
-// Prototype (Chain)
+// prototype (chain)
 
 let myName = "Emanuel";
 console.log(myName.__proto__);
@@ -8,30 +6,37 @@ console.log(myName.__proto__);
 console.log(myName.length);
 console.log(myName.toUpperCase());
 console.log(myName.toLowerCase());
+
+//  ["A", "B", "C"]
+//   0     1    2
+//  -3    -2   -1
+
+console.log(myName.slice(2));
+console.log(myName.slice(-2));
 console.log(myName.slice(4, -2)); // accept negative values
 console.log(myName.substring(2, 4));
 
 console.log(myName[1]);
 
 myName[1] = "T";
-console.log(myName); // string immutable
+console.log(myName); // string immutable *
 
-myName = "test";
+myName = "other string";
 console.log(myName);
 
-let phrase = "  I'm a Web Developer";
+let phrase = "  I'm a web developer";
 
-console.log(phrase.replace("Web", "JavaScript"));
-console.log(phrase.indexOf("W"));
+console.log(phrase.replace("web", "JavaScript"));
+console.log(phrase.indexOf("w"));
 console.log(phrase.trim());
 
 console.log(phrase.split(" ")[2]);
 console.log(phrase.split(" ").join("-"));
 console.log(phrase.split("").reverse().join(""));
 
-console.log(phrase.includes("web")); //case-sensitive
+console.log(phrase.includes("web")); // case-sensitive
 console.log("Web" == "web");
-console.log(phrase.toLocaleLowerCase().includes("web"));
+console.log(phrase.toLowerCase().includes("WEB".toLowerCase()));
 
 function log() {
   return "print";

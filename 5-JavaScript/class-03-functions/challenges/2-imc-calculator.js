0b40661e6f02b34d66d1273089031a6493b989fb
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-12-11 14:42:53 -0300

    update challenge text imc calculator

--- 5-JavaScript/class-03-functions/challenges/2-imc-calculator.js
+++ 5-JavaScript/class-03-functions/challenges/2-imc-calculator.js
@@ -1,6 +1,6 @@
-// Crie uma função que retorne o IMC do paciente
+// Crie uma função que calcule o IMC do usuário
 // A função deve receber os parâmetros de height e de weight;
-// Escreva na tela o IMC do paciente
+// Escreva na tela o IMC e se está bom ou rium seguindo a tabela
 
 function calcIMC(height, weight) {
   const imc = weight / (height / 100) ** 2;

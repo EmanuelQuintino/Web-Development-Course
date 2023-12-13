08c53f8d5f0de5aa464053d2d9cf8f2d29096fa1
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-12-13 16:26:06 -0300

    update class date examples

--- 5-JavaScript/class-04-data-manipulation/3-date.js
+++ 5-JavaScript/class-04-data-manipulation/3-date.js
@@ -1,47 +1,35 @@
 // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
 
 // new: left-hand-side expression
-let dateNow = new Date();
-let date1 = new Date("2023-09-27 12:03:19");
-let date2 = new Date("2023-09-29 12:03:19");
-
-console.log(Math.abs(date1 - date2) / 1000 / 60 / 60 / 24);
+const dateNow = new Date();
 
 console.log(dateNow);
-console.log(date1);
+console.log(new Date().getTime());
 
-console.log(date2.toLocaleString("pt-BR"));
-console.log(date2.toLocaleDateString("pt-BR"));
-console.log(date2.toLocaleTimeString("pt-BR"));
+console.log(dateNow.toLocaleString("pt-BR"));
+console.log(dateNow.toLocaleDateString("pt-BR"));
+console.log(dateNow.toLocaleTimeString("pt-BR"));
 
-let day = dateNow.getDay();
-let date = String(dateNow.getDate()).padStart(2, "0");
-let month = String(dateNow.getMonth()).padStart(2, "0");
-let year = dateNow.getFullYear();
-
-console.log(day); // 0 - 6
-console.log(date);
-console.log(month); // 0 - 11
-console.log(year);
-
-console.log(`${date}/${month}/${year}`);
+console.log(dateNow.getDay()); // 0 - 6
+console.log(dateNow.getDate());
+console.log(dateNow.getMonth()); // 0 - 11
+console.log(dateNow.getFullYear());
 
 dateNow.setDate(dateNow.getDate() + 30);
-dateNow.setMonth(dateNow.getMonth() + 2);
-dateNow.setFullYear(dateNow.getFullYear() + 3);
+dateNow.setMonth(dateNow.getMonth() + 12);
+dateNow.setFullYear(dateNow.getFullYear() + 10);
 
 console.log(dateNow.toLocaleDateString("pt-BR"));
 
-let hour = String(dateNow.getHours()).padStart(2, "0");
-let minutes = String(dateNow.getMinutes()).padStart(2, "0");
-let seconds = String(dateNow.getSeconds()).padStart(2, "0");
-let milliseconds = String(dateNow.getMilliseconds()).padStart(3, "0");
+console.log(dateNow.getHours());
+console.log(dateNow.getMinutes());
+console.log(dateNow.getSeconds());
+console.log(dateNow.getMilliseconds());
 
-console.log(`${hour}:${minutes}:${seconds}:${milliseconds}`);
-
-dateNow.setHours(dateNow.getHours() + 1);
+dateNow.setHours(dateNow.getHours() + 23);
 dateNow.setMinutes(dateNow.getMinutes() + 60);
 dateNow.setSeconds(dateNow.getSeconds() + 60);
+dateNow.setMilliseconds(dateNow.getMilliseconds() + 1000);
 
 console.log(dateNow.toLocaleTimeString("pt-BR"));
 
@@ -53,3 +41,7 @@ console.log(
     year: "numeric",
   })
 );
+
+const date1 = new Date("2023-12-27");
+const date2 = new Date("2023-09-25");
+console.log((date1 - date2) / 1000 / 60 / 60 / 24); // days
--- 5-JavaScript/class-04-data-manipulation/challenges/4-age-calculate.js
+++ 5-JavaScript/class-04-data-manipulation/challenges/4-age-calculate.js
@@ -1,8 +1,8 @@
-// ⚠️ Desafio: Crie um programa que pergunte em que data o usuário nasceu, depois escreva na tela quantos anos ele tem
+// ⚠️ Desafio: Crie um programa que pergunte em que data (yyyy-mm-dd) o usuário nasceu, depois escreva na tela quantos anos ele tem
 
 const dateWasBorn = new Date(prompt("Digite a data (yyyy-mm-dd) em que você nasceu:"));
 
 const dateNow = new Date();
-const ageUser = (dateNow - dateWasBorn) / 1000 / 60 / 60 / 24 / 365.25;
+const ageUser = Math.floor((dateNow - dateWasBorn) / 1000 / 60 / 60 / 24 / 365.25);
 
-alert(`Em você tem ${Math.floor(ageUser)} anos!`);
+alert(`Em você tem ${ageUser} anos!`);

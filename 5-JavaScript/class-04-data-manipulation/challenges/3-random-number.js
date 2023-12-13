333708c9e1ce2fbdb4ab739474cdc26aeb4e7464
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-12-13 14:27:41 -0300

    refactor challenge random number

--- 5-JavaScript/class-04-data-manipulation/challenges/3-random-number.js
+++ 5-JavaScript/class-04-data-manipulation/challenges/3-random-number.js
@@ -1,7 +1,8 @@
 // ⚠️ Desafio: Crie uma função capaz de retornar um número aleatório dentro de um intervalo determinado por parâmetros min(mínimo) e max(máximo).
 
 function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  const amount = max - min + 1;
+  return Math.floor(Math.random() * amount + min);
 }
 
-console.log(randomNumber(1, 10));
+console.log(randomNumber(2, 4));

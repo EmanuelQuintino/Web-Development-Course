8ea56627d8cec8d51283c0c86ccad4ef9afd8002
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-12-14 17:28:21 -0300

    update reset format code

--- 6-React/class-06-css-in-js/src/styles/reset.ts
+++ 6-React/class-06-css-in-js/src/styles/reset.ts
@@ -1,22 +1,22 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyles = createGlobalStyle`
- :root {
-   font-size: 62.5%;
-   font-synthesis: none;
-   text-rendering: optimizeLegibility;
-   -webkit-font-smoothing: antialiased;
-   -moz-osx-font-smoothing: grayscale;
+  :root {
+    font-size: 62.5%;
+    font-synthesis: none;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 
   * {
-   margin: 0;
-   padding: 0;
-   box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
   }
 
   body {
-   background: ${({ theme }) => theme.colors.backgroundPage};
-   color: ${({ theme }) => theme.colors.text};
+    background: ${({ theme }) => theme.colors.backgroundPage};
+    color: ${({ theme }) => theme.colors.text};
   }
 `;

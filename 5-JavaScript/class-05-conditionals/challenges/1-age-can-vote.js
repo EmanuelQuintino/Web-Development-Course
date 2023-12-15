bff3bdbcbc7b2cf3b1a241eac2f2a3af477ae57b
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-12-15 15:19:19 -0300

    update if else challenge example

--- 5-JavaScript/class-05-conditionals/challenges/1-age-can-vote.js
+++ 5-JavaScript/class-05-conditionals/challenges/1-age-can-vote.js
@@ -2,12 +2,10 @@
 
 const userAge = 18;
 
-if (userAge >= 16) {
-  if (userAge >= 18 && userAge <= 70) {
-    console.log("Voto obrigatório!");
-  } else {
-    console.log("Voto facultativo!");
-  }
+if ((userAge >= 16 && userAge < 18) || userAge > 70) {
+  console.log("Facultativo");
+} else if (userAge >= 18) {
+  console.log("Vota!");
 } else {
-  console.log("Não pode votar!");
+  console.log("Não pode!");
 }

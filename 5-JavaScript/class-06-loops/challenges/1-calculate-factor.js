87623029c1d0d559058e98183f286a904062d0c0
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-12-21 14:27:41 -0300

    update class calculate factor

--- 5-JavaScript/class-06-loops/challenges/1-calculate-factor.js
+++ 5-JavaScript/class-06-loops/challenges/1-calculate-factor.js
@@ -3,18 +3,13 @@
 
 function calculateFactor(number) {
   let result = 1;
-
   for (let i = number; i > 0; i--) {
     result *= i;
   }
-
   return result;
 
   // if (number === 0 || number === 1) return 1;
   // return number * calculateFactor(number - 1);
 }
 
-const userNumber = Number(prompt("Digite um número para calcular seu fatorial:"));
-const resultCalculateFactor = calculateFactor(userNumber);
-
-document.write(`O fatorial de ${userNumber} é: ${resultCalculateFactor}`);
+console.log(calculateFactor(5));
--- 5-JavaScript/class-06-loops/challenges/4-mega-sena.js
+++ 5-JavaScript/class-06-loops/challenges/4-mega-sena.js
@@ -8,8 +8,12 @@ while (numbersMega.length < 6) {
   let numberDrawn = String(Math.ceil(Math.random() * 60)).padStart(2, 0);
 
   if (!numbersMega.includes(numberDrawn)) {
-    numbersMega.push(numberDrawn);
+    if (numberDrawn > numbersMega[numbersMega.length - 1]) {
+      numbersMega.push(numberDrawn);
+    } else if (numberDrawn < numbersMega[0]) {
+      numbersMega.unshift(numberDrawn);
+    }
   }
 }
 
-document.write(numbersMega.sort((a, b) => a - b).join("-"));
+document.write(numbersMega.join("-"));

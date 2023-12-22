b191c28ed6642545670ec060389186e183707891
Emanuel Quintino <emanuelquintino@hotmail.com>
2023-12-22 09:01:49 -0300

    update class reduce

--- 5-JavaScript/class-07-array-methods/4-reduce.js
+++ 5-JavaScript/class-07-array-methods/4-reduce.js
@@ -1,15 +1,13 @@
 // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
 
-// sum
 const array = [1, 2, 3, 4, 5];
 console.log(array.reduce((prev, number) => prev + number, 10));
 
-// categories
-const fruits = ["maçã", "banana", "maçã", "laranja", "banana", "morango"];
+const arrayLetters = ["A", "A", "B", "C", "C", "C"];
 
-const countByFruits = fruits.reduce((prev, fruit) => {
-  prev[fruit] = (prev[fruit] || 0) + 1;
+const countLetters = arrayLetters.reduce((prev, letter) => {
+  prev[letter] ? prev[letter]++ : (prev[letter] = 1);
   return prev;
 }, {});
 
-console.log(countByFruits);
+console.log(countLetters);

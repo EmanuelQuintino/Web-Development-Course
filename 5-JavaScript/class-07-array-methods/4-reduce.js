ae8ffb6bf004e772cf5e4c7a43f5ea16f8d39d8e
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-01-05 14:48:11 -0300

    update class reduce examples

--- 5-JavaScript/class-07-array-methods/4-reduce.js
+++ 5-JavaScript/class-07-array-methods/4-reduce.js
@@ -1,7 +1,17 @@
 // Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
 
 const array = [1, 2, 3, 4, 5];
-console.log(array.reduce((prev, number) => prev + number, 10));
+console.log(array.reduce((a, b) => a + b, 100));
+console.log(array.reduce((a, b) => a * b, 100));
+
+const students = [
+  { id: 1, name: "Emanuel", salary: 8000 },
+  { id: 2, name: "Maikel", salary: 9000 },
+  { id: 3, name: "Luiza", salary: 8000 },
+  { id: 4, name: "Natanael", salary: 7000 },
+];
+
+console.log(students.reduce((a, b) => a + b.salary, 0));
 
 const arrayLetters = ["A", "A", "B", "C", "C", "C"];
 

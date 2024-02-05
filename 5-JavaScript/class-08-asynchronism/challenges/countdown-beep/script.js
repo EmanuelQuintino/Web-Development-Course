d9495261613a9b9ddc5a5dbdb3a25a81410506ae
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-02-05 07:59:03 -0300

    update format code

--- 5-JavaScript/class-08-asynchronism/challenges/countdown-beep/script.js
+++ 5-JavaScript/class-08-asynchronism/challenges/countdown-beep/script.js
@@ -1,77 +1,77 @@
-const timer = document.querySelector('.timer')
-const semicircules = document.querySelectorAll('.semicircle')
-const startButton = document.querySelector('.start-button')
+const timer = document.querySelector(".timer");
+const semicircules = document.querySelectorAll(".semicircle");
+const startButton = document.querySelector(".start-button");
 
-const beepAudio = document.querySelector('.beep-audio')
-const alertAudio = document.querySelector('.alert-audio')
+const beepAudio = document.querySelector(".beep-audio");
+const alertAudio = document.querySelector(".alert-audio");
 
-startButton.addEventListener('click', () => {
-    let time = Number(prompt('Digite quantos minutos: ')) * 60
-    let minute = Math.floor(time / 60)
-    let second = time % 60
+startButton.addEventListener("click", () => {
+  let time = Number(prompt("Digite quantos minutos: ")) * 60;
+  let minute = Math.floor(time / 60);
+  let second = time % 60;
 
-    const minutes = minute * 60000
-    const seconds = second * 1000
-    const setTime = minutes + seconds
-    const startTime = Date.now();
-    const futureTime = startTime + setTime
+  const minutes = minute * 60000;
+  const seconds = second * 1000;
+  const setTime = minutes + seconds;
+  const startTime = Date.now();
+  const futureTime = startTime + setTime;
 
-    timer.style.display = 'flex'
-    startButton.style.display = 'none'
+  timer.style.display = "flex";
+  startButton.style.display = "none";
 
-    function showTimer() {
-        minute = Math.floor(time / 60)
-        second = time % 60
-        timer.innerHTML = `${String(minute).padStart(2, 0)}:${String(second).padStart(2, 0)}`
+  function showTimer() {
+    minute = Math.floor(time / 60);
+    second = time % 60;
+    timer.innerHTML = `${String(minute).padStart(2, 0)}:${String(second).padStart(2, 0)}`;
+  }
+
+  const timerRotate = setInterval(() => {
+    const currentTime = Date.now();
+    const remainingTime = futureTime - currentTime;
+    const angle = (remainingTime / setTime) * 360;
+
+    if (angle > 180) {
+      semicircules[2].style.display = "none";
+      semicircules[0].style.transform = "rotate(180deg)";
+      semicircules[1].style.transform = `rotate(${angle}deg)`;
+    } else {
+      semicircules[2].style.display = "block";
+      semicircules[0].style.transform = `rotate(${angle}deg)`;
+      semicircules[1].style.transform = `rotate(${angle}deg)`;
     }
 
-    const timerRotate = setInterval(() => {
-        const currentTime = Date.now()
-        const remainingTime = futureTime - currentTime
-        const angle = (remainingTime / setTime) * 360
+    if (remainingTime <= 10000) {
+      semicircules[0].style.backgroundColor = "red";
+      semicircules[1].style.backgroundColor = "red";
+      timer.style.color = "red";
+    }
 
-        if (angle > 180) {
-            semicircules[2].style.display = 'none'
-            semicircules[0].style.transform = 'rotate(180deg)'
-            semicircules[1].style.transform = `rotate(${angle}deg)`
-        } else {
-            semicircules[2].style.display = 'block'
-            semicircules[0].style.transform = `rotate(${angle}deg)`
-            semicircules[1].style.transform = `rotate(${angle}deg)`
-        }
+    if (remainingTime < 0) {
+      semicircules[0].style.backgroundColor = "#088b8b";
+      semicircules[1].style.backgroundColor = "#088b8b";
+      timer.style.color = "#088b8b";
 
-        if (remainingTime <= 10000) {
-            semicircules[0].style.backgroundColor = 'red'
-            semicircules[1].style.backgroundColor = 'red'
-            timer.style.color = 'red'
-        }
+      timer.style.display = "none";
+      startButton.style.display = "flex";
 
-        if (remainingTime < 0) {
-            semicircules[0].style.backgroundColor = '#088b8b'
-            semicircules[1].style.backgroundColor = '#088b8b'
-            timer.style.color = '#088b8b'
+      clearInterval(timerRotate);
+    }
+  }, 10);
 
-            timer.style.display = 'none'
-            startButton.style.display = 'flex'
+  const countDown = setInterval(() => {
+    if (time <= 11 && time != 1) {
+      beepAudio.play();
+    }
 
-            clearInterval(timerRotate)
-        }
-    }, 10);
+    if (time == 1) {
+      alertAudio.play();
 
-    const countDown = setInterval(() => {
-        if (time <= 11 && time != 1) {
-            beepAudio.play();
-        }
+      clearInterval(countDown);
+    } else {
+      --time;
+    }
+    showTimer();
+  }, 1000);
 
-        if (time == 1) {
-            alertAudio.play()
-
-            clearInterval(countDown)
-        } else {
-            --time
-        }
-        showTimer()
-    }, 1000)
-
-    showTimer()
-})
+  showTimer();
+});
--- 5-JavaScript/class-08-asynchronism/challenges/countdown-new-year/script.js
+++ 5-JavaScript/class-08-asynchronism/challenges/countdown-new-year/script.js
@@ -1,22 +1,22 @@
-const nextYear = new Date().getFullYear() + 1
-const nextYearTime = new Date(`${nextYear}-01-01 00:00:00`)
+const nextYear = new Date().getFullYear() + 1;
+const nextYearTime = new Date(`${nextYear}-01-01 00:00:00`);
 
 const countdown = setInterval(() => {
-    const dateNow = new Date()
-    const timeLeft = nextYearTime - dateNow
-    const days = Math.floor(timeLeft / 1000 / 60 / 60 / 24)
-    const hours = Math.floor(timeLeft / 1000 / 60 / 60) % 24
-    const minutes = Math.floor(timeLeft / 1000 / 60) % 60
-    const seconds = Math.floor(timeLeft / 1000) % 60
+  const dateNow = new Date();
+  const timeLeft = nextYearTime - dateNow;
+  const days = Math.floor(timeLeft / 1000 / 60 / 60 / 24);
+  const hours = Math.floor(timeLeft / 1000 / 60 / 60) % 24;
+  const minutes = Math.floor(timeLeft / 1000 / 60) % 60;
+  const seconds = Math.floor(timeLeft / 1000) % 60;
 
-    const displayDays = document.querySelector('.days')
-    const displayHours = document.querySelector('.hours')
-    const displayMinutes = document.querySelector('.minutes')
-    const displaySeconds = document.querySelector('.seconds')
+  const displayDays = document.querySelector(".days");
+  const displayHours = document.querySelector(".hours");
+  const displayMinutes = document.querySelector(".minutes");
+  const displaySeconds = document.querySelector(".seconds");
 
-    const displayTime = (time, length) => String(time).padStart(length, 0)
-    displayDays.innerHTML = displayTime(days, 3) 
-    displayHours.innerHTML = displayTime(hours, 2) 
-    displayMinutes.innerHTML = displayTime(minutes, 2)  
-    displaySeconds.innerHTML = displayTime(seconds, 2)  
-}, 1000)
+  const displayTime = (time, length) => String(time).padStart(length, 0);
+  displayDays.innerHTML = displayTime(days, 3);
+  displayHours.innerHTML = displayTime(hours, 2);
+  displayMinutes.innerHTML = displayTime(minutes, 2);
+  displaySeconds.innerHTML = displayTime(seconds, 2);
+}, 1000);

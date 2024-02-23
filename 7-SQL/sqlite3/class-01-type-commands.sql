153d17926231ae8c3678b1250d468b1bd6fccfbd
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-02-23 00:15:28 -0300

    update class type commands

--- 7-SQL/sqlite3/class-01-type-commands.sql
+++ 7-SQL/sqlite3/class-01-type-commands.sql
@@ -2,16 +2,17 @@
 CREATE TABLE IF NOT EXISTS students (
     id INTEGER PRIMARY KEY AUTOINCREMENT, 
     name TEXT NOT NULL,
+    cpf TEXT NOT NULL UNIQUE,
     age NUMERIC
 );
 
 DROP TABLE students;
 
 -- DML
-INSERT INTO students (name, age) VALUES 
-    ("Wellington", 18),
-    ("Emanuel", 18), 
-    ("Maurício", 21);
+INSERT INTO students (name, cpf, age) VALUES 
+    ("Wellington", "123.123.123-01", 18),
+    ("Emanuel","123.123.123-02", 18), 
+    ("Maurício", "123.123.123-03", 21);
 
 -- DQL
 SELECT * FROM students;
@@ -27,6 +28,11 @@ SELECT *
 FROM students 
 WHERE age > 18 
 ORDER BY age DESC;
+
+-- TCL
+BEGIN TRANSACTION;
+COMMIT;
+ROLLBACK;
 
 -- DML
 UPDATE students 
@@ -46,7 +52,8 @@ DELETE FROM students WHERE id = 4;
 
 -- DDL
 ALTER TABLE students 
-ADD COLUMN city TEXT;
+ADD COLUMN city TEXT 
+NOT NULL DEFAULT "Jucás";
 
 ALTER TABLE students 
 DROP COLUMN city;

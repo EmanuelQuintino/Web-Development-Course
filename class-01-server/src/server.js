e35d2dd3abe9f8676a411af5ac796ebcbc3484d3
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-03-11 11:24:43 -0300

    remove class 01 server

--- class-01-server/src/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.listen(port, console.log(`Server is running on port ${port}...`));
-
-app.route('/').get((request, response) => response.send('Class 01 - Server'));

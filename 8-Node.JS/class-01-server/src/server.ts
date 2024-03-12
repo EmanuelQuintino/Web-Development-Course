bee9e9032261e9dcbc4ed0b77524122c061e2291
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-03-11 22:58:22 -0300

    update server is running message

--- 8-Node.JS/class-01-server/src/server.ts
+++ 8-Node.JS/class-01-server/src/server.ts
@@ -3,7 +3,9 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.listen(port, () => console.log(`Server is running on port ${port}...`));
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
 
 app.route("/").get((_request, response) => {
   response.send({ className: "Class 01 Server" });
--- 8-Node.JS/class-02-routes/src/server.ts
+++ 8-Node.JS/class-02-routes/src/server.ts
@@ -3,7 +3,9 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.listen(port, () => console.log(`Server is running on PORT ${port}...`));
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
 
 // Middleware
 app.use(express.json());

9fd9a3c008fa00f63131777a6f4775b04380119b
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-03-17 18:05:53 -0300

    add sqlite connection

--- 8-Node.JS/class-04-databases/sqlite3/src/server.ts
+++ 8-Node.JS/class-04-databases/sqlite3/src/server.ts
@@ -1,8 +1,17 @@
 import express from "express";
+import { sqliteConnection } from "./databases/sqlite3";
+import { router } from "./routes";
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(router);
+
 app.listen(port, () => {
-  console.log(`Server is runninig on port ${port}...`);
+  console.log(`Server is runninig on port ${port}!`);
 });
+
+sqliteConnection()
+  .then(() => console.log("Database is connected..."))
+  .catch((error) => console.error("Error! Database is not connected: ", error));

e312e54e60b57fb71041bba042884ba9e7cd13d7
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-03-17 17:34:09 -0300

    add user routes

---
+++ 8-Node.JS/class-04-databases/sqlite3/src/controllers/userControllers.ts
@@ -0,0 +1,17 @@
+export const userControllers = {
+  create() {
+    console.log("create");
+  },
+
+  read() {
+    console.log("read");
+  },
+
+  update() {
+    console.log("update");
+  },
+
+  delete() {
+    console.log("delete");
+  },
+};
---
+++ 8-Node.JS/class-04-databases/sqlite3/src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { userControllers } from "../controllers/userControllers";
+
+export const router = Router();
+
+router.post("/user", userControllers.create);
+router.get("/user/:id", userControllers.read);
+router.get("/user", userControllers.read);
+router.put("/user/:id", userControllers.update);
+router.delete("/user/:id", userControllers.delete);

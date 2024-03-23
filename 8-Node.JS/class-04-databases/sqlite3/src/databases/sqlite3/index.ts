665ff27ab1ab4db796bc4ce39a2716af63dbf2a6
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-03-23 09:36:52 -0300

    add return type sqlite database

--- 8-Node.JS/class-04-databases/sqlite3/src/databases/sqlite3/index.ts
+++ 8-Node.JS/class-04-databases/sqlite3/src/databases/sqlite3/index.ts
@@ -2,11 +2,11 @@ import sqlite3 from "sqlite3";
 import * as sqlite from "sqlite";
 import path from "path";
 
-export async function sqliteConnection(): Promise<sqlite.Database> {
+export async function sqliteConnection() {
   const database = await sqlite.open({
     filename: path.resolve(__dirname, "..", "database.db"),
     driver: sqlite3.Database,
   });
 
-  return database;
+  return database as sqlite.Database;
 }

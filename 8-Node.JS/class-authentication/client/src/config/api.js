3ddf3ad91b6f76abb714257dadcc521f44a5dd24
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-03-24 00:43:51 -0300

    remove old class authentication

--- 8-Node.JS/class-authentication/client/src/config/api.js
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-export const API = axios.create({
-    baseURL: "http://localhost:3000"
-});
--- 8-Node.JS/class-authentication/server/src/config/auth.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    secret: process.env.SECRET_TOKEN,
-    expiresIn: process.env.EXPIRESIN_TOKEN
-}

d4c15722f4a58506fa0ddf0c3184c2e935cd5b24
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-03-29 12:15:49 -0300

    add middleware authorization roles

--- 8-Node.JS/class-05-authentication/src/controllers/sales.ts
+++ 8-Node.JS/class-05-authentication/src/controllers/sales.ts
@@ -3,7 +3,7 @@ import { Request, Response } from "express";
 export const salesControllers = {
   read(_req: Request, res: Response) {
     return res.status(200).json({
-      report: {
+      reports: {
         sale1: "10000",
         sale2: "20000",
         sale3: "30000",

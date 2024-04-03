afb850c015415b3def14ce86d7d8d40e587f4795
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-04-02 23:09:32 -0300

    remove class automatic tests

--- 8-Node.JS/class-automatic-tests/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  bail: true,
-  clearMocks: true,
-  collectCoverage: true,
-  coverageProvider: "v8",
-  coverageDirectory: "coverage",
-  testMatch: ["<rootDir>/services/**/*.test.js"]
-};
--- 8-Node.JS/class-automatic-tests/services/evenOrOdd.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    check (num) {
-        if (num % 2 == 0) {
-            return 'even';
-        } else {
-            return 'odd';
-        }
-    }
-}

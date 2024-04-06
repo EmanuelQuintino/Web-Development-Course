1b02f87028557e2f263a18eb8d88165cb024ba12
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-04-05 21:53:25 -0300

    update zod validation messages

--- 8-Node.JS/class-05-authentication/src/controllers/userControllers.ts
+++ 8-Node.JS/class-05-authentication/src/controllers/userControllers.ts
@@ -5,35 +5,37 @@ import { z } from "zod";
 export const userControllers = {
   async create(req: Request, res: Response, next: NextFunction) {
     try {
-      const userSchema = z.object({
-        name: z
-          .string({
-            required_error: "name is required!",
-            invalid_type_error: "name must be a string!",
-          })
-          .min(3, { message: "name must have at least 3 characters" })
-          .max(255, { message: "max name length exceeded" }),
+      const userSchema = z
+        .object({
+          name: z
+            .string({
+              required_error: "name is required!",
+              invalid_type_error: "name must be a string!",
+            })
+            .min(3, "name must have at least 3 characters")
+            .max(255, "max name length exceeded"),
 
-        email: z
-          .string({
-            required_error: "email is required!",
-            invalid_type_error: "email must be a string!",
-          })
-          .email("invalid email!")
-          .max(255, { message: "max email length exceeded" }),
+          email: z
+            .string({
+              required_error: "email is required!",
+              invalid_type_error: "email must be a string!",
+            })
+            .email("invalid email!")
+            .max(255, "max email length exceeded"),
 
-        password: z
-          .string({
-            required_error: "email is required!",
-            invalid_type_error: "password must be a string!",
-          })
-          .min(7, { message: "password must have at least 7 characters" })
-          .regex(/^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{7,}$/, {
-            message:
-              "The password must contain at least one capital letter, one number and one special character.",
-          })
-          .max(255, { message: "max password length exceeded" }),
-      });
+          password: z
+            .string({
+              required_error: "email is required!",
+              invalid_type_error: "password must be a string!",
+            })
+            .min(7, "password must have at least 7 characters")
+            .max(255, "max password length exceeded")
+            .regex(/^(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z\d]).{7,}$/, {
+              message:
+                "The password must contain at least one capital letter, one number and one special character.",
+            }),
+        })
+        .strict();
 
       const { name, email, password } = userSchema.parse(req.body);
 

b65557ad78934e739230ec842a3113adac16bb12
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-04-30 18:10:35 -0300

    add insomnia routes

--- 8-Node.JS/class-02-routes/src/routes/index.ts
+++ 8-Node.JS/class-02-routes/src/routes/index.ts
@@ -3,11 +3,11 @@ import { Router, Request, Response } from "express";
 export const router = Router();
 
 router.get("/", (_req: Request, res: Response) => {
-  res.send({ className: "Class 02 Routes" });
+  res.json({ className: "Class 02 Routes" });
 });
 
 router.get("/users", (_req: Request, res: Response) => {
-  res.send([
+  res.json([
     { id: 1, name: "Goku" },
     { id: 2, name: "Gohan" },
     { id: 3, name: "Bulma" },
@@ -17,18 +17,18 @@ router.get("/users", (_req: Request, res: Response) => {
 // route params
 router.get("/user/:id/:name", (req: Request, res: Response) => {
   const { id, name } = req.params;
-  res.send({ id, name });
+  res.json({ id, name });
 });
 
 // query params
 router.get("/user", (req: Request, res: Response) => {
   const { id, name } = req.query;
-  res.send({ id, name });
+  res.json({ id, name });
 });
 
 // body params
 router.post("/user", (req: Request, res: Response) => {
   const { id, name, age } = req.body;
   console.log({ id, name, age });
-  res.send({ status: "user create!" });
+  res.json({ status: "user create!" });
 });

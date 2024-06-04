0802e68ef0ab3747b7a9f09ff37dff9cb0864d43
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-06-04 19:53:45 -0300

    rename challenges and cohesion example

--- 5-JavaScript/class-01-data-types/2-template-literals.js
+++ 5-JavaScript/class-01-data-types/2-template-literals.js
@@ -16,7 +16,7 @@ let num2 = 20;
 num1 = Number(num1); // type conversion (typecasting)
 
 console.log(num1 + num2);
-console.log(10 + "20"); // type coersion
+console.log(10 + "20"); // type cohesion
 console.log(Number("10") + 20);
 console.log(+"10" + 20);
 

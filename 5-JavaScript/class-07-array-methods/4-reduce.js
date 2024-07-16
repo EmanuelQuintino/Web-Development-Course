0c0cfcd3dc91ffda56ffed3f6e1795ac45d14b67
Emanuel Quintino <emanuelquintino@hotmail.com>
2024-07-16 19:29:23 -0300

    add group reduce example

--- 5-JavaScript/class-07-array-methods/4-reduce.js
+++ 5-JavaScript/class-07-array-methods/4-reduce.js
@@ -5,13 +5,24 @@ console.log(array.reduce((a, b) => a + b, 100));
 console.log(array.reduce((a, b) => a * b, 100));
 
 const students = [
-  { id: 1, name: "Emanuel", salary: 8000 },
-  { id: 2, name: "Maikel", salary: 9000 },
-  { id: 3, name: "Luiza", salary: 8000 },
-  { id: 4, name: "Natanael", salary: 7000 },
+  { id: 1, name: "Emanuel", salary: 8000, cite: "Jucás" },
+  { id: 2, name: "Maikel", salary: 9000, cite: "Jucás" },
+  { id: 3, name: "Luiza", salary: 8000, cite: "Cariús" },
+  { id: 4, name: "Natanael", salary: 7000, cite: "Cariús" },
+  { id: 5, name: "Odilon", salary: 6000, cite: "Ju´cas" },
 ];
 
 console.log(students.reduce((a, b) => a + b.salary, 0));
+
+const groupeByCity = students.reduce((acc, student) => {
+  if (!acc[student.cite]) {
+    acc[student.cite] = [];
+  }
+  acc[student.cite].push(student);
+  return acc;
+}, {});
+
+console.log(groupeByCity);
 
 const arrayLetters = ["A", "A", "B", "C", "C", "C"];
 
